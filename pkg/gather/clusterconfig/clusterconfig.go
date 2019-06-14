549f8c6408e01d420e286b0cbbbb8c98ecb0dcc6
Clayton Coleman <ccoleman@redhat.com>
2019-06-14 16:55:14 -0400

    Gather data from cluster operators to support operator as well
    Also write the cluster ID to its own file, and serialize type meta
    along with the JSON objects.

--- pkg/gather/clusterconfig/clusterconfig.go
+++ pkg/gather/clusterconfig/clusterconfig.go
@@ -2,7 +2,6 @@ package clusterconfig
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"regexp"
 	"sort"
@@ -13,10 +12,13 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 
 	configv1 "github.com/openshift/api/config/v1"
+	"github.com/openshift/client-go/config/clientset/versioned/scheme"
 	configv1client "github.com/openshift/client-go/config/clientset/versioned/typed/config/v1"
 
 	"github.com/openshift/support-operator/pkg/record"
 )
+
+var serializer = scheme.Codecs.LegacyCodec(configv1.SchemeGroupVersion)
 
 type Gatherer struct {
 	client configv1client.ConfigV1Interface
@@ -31,8 +33,21 @@ func New(client configv1client.ConfigV1Interface) *Gatherer {
 	}
 }
 
+var reInvalidUIDCharacter = regexp.MustCompile(`[^a-z0-9\-]`)
+
 func (i *Gatherer) Gather(ctx context.Context, recorder record.Interface) error {
 	return records(recorder,
+		func() ([]record.Record, []error) {
+			config, err := i.client.ClusterOperators().List(metav1.ListOptions{})
+			if err != nil {
+				return nil, []error{err}
+			}
+			records := make([]record.Record, 0, len(config.Items))
+			for i := range config.Items {
+				records = append(records, record.Record{Name: fmt.Sprintf("config/clusteroperator/%s", config.Items[i].Name), Fingerprint: config.Items[i].ResourceVersion, Item: ClusterOperatorAnonymizer{&config.Items[i]}})
+			}
+			return records, nil
+		},
 		func() (record.Record, error) {
 			config, err := i.client.ClusterVersions().Get("version", metav1.GetOptions{})
 			if err != nil {
@@ -40,6 +55,13 @@ func (i *Gatherer) Gather(ctx context.Context, recorder record.Interface) error 
 			}
 			i.setClusterVersion(config)
 			return record.Record{Name: "config/version", Fingerprint: config.ResourceVersion, Item: ClusterVersionAnonymizer{config}}, nil
+		},
+		func() (record.Record, error) {
+			version := i.ClusterVersion()
+			if version == nil {
+				return record.Record{}, errSkipRecord
+			}
+			return record.Record{Name: "config/id", Fingerprint: version.ResourceVersion, Item: Raw{string(version.Spec.ClusterID)}}, nil
 		},
 		func() (record.Record, error) {
 			config, err := i.client.Infrastructures().Get("cluster", metav1.GetOptions{})
@@ -79,12 +101,28 @@ func (i *Gatherer) Gather(ctx context.Context, recorder record.Interface) error 
 	)
 }
 
-func records(recorder record.Interface, fns ...func() (record.Record, error)) error {
+var errSkipRecord = fmt.Errorf("skip recording")
+
+func records(recorder record.Interface, bulkFn func() ([]record.Record, []error), fns ...func() (record.Record, error)) error {
 	var errors []string
+	if bulkFn != nil {
+		records, errs := bulkFn()
+		for _, err := range errs {
+			errors = append(errors, err.Error())
+		}
+		for _, record := range records {
+			if err := recorder.Record(record); err != nil {
+				errors = append(errors, fmt.Sprintf("unable to record %s: %v", record.Name, err.Error()))
+				continue
+			}
+		}
+	}
 	for _, fn := range fns {
 		record, err := fn()
 		if err != nil {
-			errors = append(errors, err.Error())
+			if err != errSkipRecord {
+				errors = append(errors, err.Error())
+			}
 			continue
 		}
 		if err := recorder.Record(record); err != nil {
@@ -117,16 +155,22 @@ func uniqueStrings(arr []string) []string {
 	return arr[:last]
 }
 
+type Raw struct{ string }
+
+func (r Raw) Marshal(_ context.Context) ([]byte, error) {
+	return []byte(r.string), nil
+}
+
 type Anonymizer struct{ runtime.Object }
 
 func (a Anonymizer) Marshal(_ context.Context) ([]byte, error) {
-	return json.Marshal(a.Object)
+	return runtime.Encode(serializer, a.Object)
 }
 
 type InfrastructureAnonymizer struct{ *configv1.Infrastructure }
 
 func (a InfrastructureAnonymizer) Marshal(_ context.Context) ([]byte, error) {
-	return json.Marshal(anonymizeInfrastructure(a.Infrastructure))
+	return runtime.Encode(serializer, anonymizeInfrastructure(a.Infrastructure))
 }
 
 func anonymizeInfrastructure(config *configv1.Infrastructure) *configv1.Infrastructure {
@@ -140,30 +184,33 @@ type ClusterVersionAnonymizer struct{ *configv1.ClusterVersion }
 
 func (a ClusterVersionAnonymizer) Marshal(_ context.Context) ([]byte, error) {
 	a.ClusterVersion.Spec.Upstream = configv1.URL(anonymizeURL(string(a.ClusterVersion.Spec.Upstream)))
-	return json.Marshal(a.ClusterVersion)
+	return runtime.Encode(serializer, a.ClusterVersion)
 }
 
 type IngressAnonymizer struct{ *configv1.Ingress }
 
 func (a IngressAnonymizer) Marshal(_ context.Context) ([]byte, error) {
 	a.Ingress.Spec.Domain = anonymizeURL(a.Ingress.Spec.Domain)
-	return json.Marshal(a.Ingress)
+	return runtime.Encode(serializer, a.Ingress)
 }
 
 var reURL = regexp.MustCompile(`[^\.\-/\:]`)
 
 func anonymizeURL(s string) string { return reURL.ReplaceAllString(s, "x") }
 
-// type IngressListAnonymizer struct{ *configv1.IngressList }
+type ClusterOperatorAnonymizer struct{ *configv1.ClusterOperator }
 
-// func (a IngressListAnonymizer) Marshal(_ context.Context) ([]byte, error) {
-// 	for i := range a.IngressList.Items {
-// 		a.IngressList.Items[i].Spec.Domain = anonymizeURL(a.IngressList.Items[i].Spec.Domain)
-// 	}
-// 	return json.Marshal(a.IngressList)
+func (a ClusterOperatorAnonymizer) Marshal(_ context.Context) ([]byte, error) {
+	return runtime.Encode(serializer, a.ClusterOperator)
+}
+
+// type ClusterOperatorListAnonymizer struct{ *configv1.ClusterOperatorList }
+
+// func (a ClusterOperatorListAnonymizer) Marshal(_ context.Context) ([]byte, error) {
+// 	return runtime.Encode(serializer, a.ClusterOperatorList)
 // }
 
-// func ingressListResourceVersion(items *configv1.IngressList) string {
+// func clusterOperatorListResourceVersion(items *configv1.ClusterOperatorList) string {
 // 	rvs := make([]string, 0, len(items.Items))
 // 	for _, item := range items.Items {
 // 		rvs = append(rvs, item.ResourceVersion)
