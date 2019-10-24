fea3901d5e7fe3a33a50911a14ad00b0c58167e5
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-24 15:41:14 +0200

    Better handling of empty configuration + config needs to be read from specified file at the beginning

--- pkg/instrumentation/instrumentation.go
+++ pkg/instrumentation/instrumentation.go
@@ -5,11 +5,15 @@ import (
 	"io/ioutil"
 	"k8s.io/klog"
 	"net/http"
+	"sort"
 	"time"
 )
 
+// An unstructured operator configuration that can contain
+// any data stored under (string) keys.
 type OperatorConfiguration map[string]interface{}
 
+// Constructor for the operator configuration.
 func NewOperatorConfiguration() OperatorConfiguration {
 	return make(map[string]interface{})
 }
@@ -23,7 +27,13 @@ func (configuration OperatorConfiguration) fromJSON(payload []byte) error {
 	return json.Unmarshal(payload, &configuration)
 }
 
-func (configuration OperatorConfiguration) mergeWith(other OperatorConfiguration) {
+func (configuration OperatorConfiguration) addAll(other OperatorConfiguration) {
+	for key, value := range other {
+		configuration[key] = value
+	}
+}
+
+func (configuration OperatorConfiguration) updateExisting(other OperatorConfiguration) {
 	for key, value := range other {
 		_, found := configuration[key]
 		if found {
@@ -32,19 +42,49 @@ func (configuration OperatorConfiguration) mergeWith(other OperatorConfiguration
 	}
 }
 
-func (configuration OperatorConfiguration) print(title string) {
-	klog.Info(title)
-	for key, val := range configuration {
-		klog.Info(key, "\t", val)
+func (configuration OperatorConfiguration) mergeWith(other OperatorConfiguration) {
+	if len(configuration) == 0 {
+		configuration.addAll(other)
+	} else {
+		configuration.updateExisting(other)
 	}
 }
 
-func createOriginalConfiguration() OperatorConfiguration {
+// Print the configuration. Items are sorted by its keys.
+func (configuration OperatorConfiguration) print(title string) {
+	klog.Info(title)
+	if len(configuration) == 0 {
+		klog.Info("\t* empty *")
+		return
+	}
+
+	var keys []string
+	for key, _ := range configuration {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	for _, key := range keys {
+		klog.Info("\t", key, "\t=> ", configuration[key])
+	}
+}
+
+// Create original operator configuration.
+func createOriginalConfiguration(filename string) OperatorConfiguration {
 	var cfg = NewOperatorConfiguration()
-	jsonStr := `
-{"no_op":"?",
- "watch":[]}`
-	cfg.fromJSON([]byte(jsonStr))
+
+	payload, err := ioutil.ReadFile(filename)
+	if err != nil {
+		klog.Error("Can not open configuration file: ", err)
+		// ok for now, the configuration will be simply empty
+		return cfg
+	}
+
+	err = cfg.fromJSON(payload)
+	if err != nil {
+		klog.Warning("Can not decode original configuration read from the file ", filename)
+		// ok for now, the configuration will be simply empty
+		return cfg
+	}
 	return cfg
 }
 
@@ -63,25 +103,38 @@ func retrieveConfigurationFrom(url string, cluster string) (OperatorConfiguratio
 		return nil, err
 	}
 
+	if response.StatusCode != http.StatusOK {
+		klog.Info("No configuration has been provided by the service")
+		return nil, nil
+	}
+
 	defer response.Body.Close()
-	body, _ := ioutil.ReadAll(response.Body)
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
 
 	var c2 = NewOperatorConfiguration()
-	c2.fromJSON(body)
+	err = c2.fromJSON(body)
+	if err != nil {
+		klog.Warning("Can not decode the configuration provided by the service")
+		return nil, err
+	}
 	return c2, nil
 }
 
-func StartInstrumentation(serviceUrl string, interval int) {
-	c1 := createOriginalConfiguration()
+func StartInstrumentation(serviceUrl string, interval int, clusterName string, configFile string) {
+	klog.Info("Read original configuration")
+	c1 := createOriginalConfiguration(configFile)
+	c1.print("Original configuration")
 	klog.Info("Gathering configuration each ", interval, " second(s)")
 	for {
 		klog.Info("Gathering info from service ", serviceUrl)
-		c2, err := retrieveConfigurationFrom(serviceUrl, "cluster0")
+		c2, err := retrieveConfigurationFrom(serviceUrl, clusterName)
 		if err != nil {
 			klog.Error("unable to retrieve configuration from the service")
-		} else {
+		} else if c2 != nil {
 			c2.print("Retrieved configuration")
-
 			c1.mergeWith(c2)
 			c1.print("Updated configuration")
 		}
