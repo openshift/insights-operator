d01a541eea51cfd65184169220fa22ef03344448
Angelina Nikiforova <anikifor@redhat.com>
2019-12-16 13:31:33 +0100

    added TestOptOutOptIn and moved some code to functions

--- test/integration/bugs_test.go
+++ test/integration/bugs_test.go
@@ -1,20 +1,9 @@
 package integration
 
 import (
-	"bytes"
-	"fmt"
-	"io"
-	"strings"
 	"testing"
-	"time"
 
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/wait"
-)
-
-var (
-	kubeClient = KubeClient()
 )
 
 // https://bugzilla.redhat.com/show_bug.cgi?id=1750665
@@ -28,76 +17,8 @@ func TestDefaultUploadFrequency(t *testing.T) {
 	}
 
 	// restart insights-operator (delete pods)
-	pods, err := kubeClient.CoreV1().Pods("openshift-insights").List(metav1.ListOptions{})
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-
-	for _, pod := range pods.Items {
-		kubeClient.CoreV1().Pods("openshift-insights").Delete(pod.Name, &metav1.DeleteOptions{})
-		err := wait.PollImmediate(1*time.Second, 10*time.Minute, func() (bool, error) {
-			_, err := kubeClient.CoreV1().Pods("openshift-insights").Get(pod.Name, metav1.GetOptions{})
-			if err == nil {
-				fmt.Printf("the pod is not yet deleted: %v\n", err)
-				return false, nil
-			}
-			fmt.Println("the pod is deleted")
-			return true, nil
-		})
-		fmt.Print(err)
-	}
-
-	// check new pods are created and running
-	errPod := wait.PollImmediate(1*time.Second, 10*time.Minute, func() (bool, error) {
-		newPods, _ := kubeClient.CoreV1().Pods("openshift-insights").List(metav1.ListOptions{})
-		if len(newPods.Items) == 0 {
-			fmt.Printf("pods are not yet created")
-			return false, nil
-		}
-
-		for _, newPod := range newPods.Items {
-			pod, err := kubeClient.CoreV1().Pods("openshift-insights").Get(newPod.Name, metav1.GetOptions{})
-			if err != nil {
-				panic(err.Error())
-			}
-			if pod.Status.Phase != "Running" {
-				return false, nil
-			}
-		}
-
-		fmt.Println("the pods are created")
-		return true, nil
-	})
-	fmt.Print(errPod)
+	RestartInsightsOperator(t)
 
 	// check logs for "Gathering cluster info every 2h0m0s"
-	newPods, err := kubeClient.CoreV1().Pods("openshift-insights").List(metav1.ListOptions{})
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-
-	for _, newPod := range newPods.Items {
-		pod, err := kubeClient.CoreV1().Pods("openshift-insights").Get(newPod.Name, metav1.GetOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-		req := kubeClient.CoreV1().Pods(pod.Namespace).GetLogs(pod.Name, &corev1.PodLogOptions{})
-		podLogs, err := req.Stream()
-		if err != nil {
-			panic(err.Error())
-		}
-		defer podLogs.Close()
-
-		buf := new(bytes.Buffer)
-		_, err = io.Copy(buf, podLogs)
-		if err != nil {
-			panic(err.Error())
-		}
-		log := buf.String()
-
-		result := strings.Contains(log, "Gathering cluster info every 2h0m0s")
-		if result == false {
-			t.Error("Default upload frequency is not 2 hours")
-		}
-	}
+	CheckPodsLogs(t, kubeClient, "Gathering cluster info every 2h0m0s")
 }
--- test/integration/main_test.go
+++ test/integration/main_test.go
@@ -1,16 +1,22 @@
 package integration
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 	"testing"
 	"time"
 
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
+
+var kubeClient = KubeClient()
 
 func KubeClient() (result *kubernetes.Clientset) {
 	kubeconfig, ok := os.LookupEnv("KUBECONFIG") // variable is a path to the local kubeconfig
@@ -33,8 +39,94 @@ func KubeClient() (result *kubernetes.Clientset) {
 	return kubeClient
 }
 
+func RestartInsightsOperator(t *testing.T) {
+	// restart insights-operator (delete pods)
+	pods, err := kubeClient.CoreV1().Pods("openshift-insights").List(metav1.ListOptions{})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	for _, pod := range pods.Items {
+		kubeClient.CoreV1().Pods("openshift-insights").Delete(pod.Name, &metav1.DeleteOptions{})
+		err := wait.PollImmediate(1*time.Second, 10*time.Minute, func() (bool, error) {
+			_, err := kubeClient.CoreV1().Pods("openshift-insights").Get(pod.Name, metav1.GetOptions{})
+			if err == nil {
+				fmt.Printf("the pod is not yet deleted: %v\n", err)
+				return false, nil
+			}
+			fmt.Println("the pod is deleted")
+			return true, nil
+		})
+		fmt.Print(err)
+	}
+
+	// check new pods are created and running
+	errPod := wait.PollImmediate(1*time.Second, 10*time.Minute, func() (bool, error) {
+		newPods, _ := kubeClient.CoreV1().Pods("openshift-insights").List(metav1.ListOptions{})
+		if len(newPods.Items) == 0 {
+			fmt.Printf("pods are not yet created\n")
+			return false, nil
+		}
+
+		for _, newPod := range newPods.Items {
+			pod, err := kubeClient.CoreV1().Pods("openshift-insights").Get(newPod.Name, metav1.GetOptions{})
+			if err != nil {
+				panic(err.Error())
+			}
+			if pod.Status.Phase != "Running" {
+				return false, nil
+			}
+		}
+
+		fmt.Println("the pods are created")
+		return true, nil
+	})
+	t.Log(errPod)
+}
+
+func CheckPodsLogs(t *testing.T, kubeClient *kubernetes.Clientset, message string) {
+	newPods, err := kubeClient.CoreV1().Pods("openshift-insights").List(metav1.ListOptions{})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	for _, newPod := range newPods.Items {
+		pod, err := kubeClient.CoreV1().Pods("openshift-insights").Get(newPod.Name, metav1.GetOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+
+		errLog := wait.PollImmediate(5*time.Second, 15*time.Minute, func() (bool, error) {
+			req := kubeClient.CoreV1().Pods(pod.Namespace).GetLogs(pod.Name, &corev1.PodLogOptions{})
+			podLogs, err := req.Stream()
+			if err != nil {
+				return false, nil
+			}
+			defer podLogs.Close()
+
+			buf := new(bytes.Buffer)
+			_, err = io.Copy(buf, podLogs)
+			if err != nil {
+				panic(err.Error())
+			}
+			log := buf.String()
+
+			result := strings.Contains(log, message)
+			if result == false {
+				fmt.Printf("No %s in logs\n", message)
+				return false, nil
+			}
+
+			fmt.Printf("%s found\n", message)
+			return true, nil
+		})
+		if errLog != nil {
+			t.Error(errLog)
+		}
+	}
+}
+
 func TestMain(m *testing.M) {
-	kubeClient = KubeClient()
 	// check the operator is up
 	err := waitForOperator(kubeClient)
 	if err != nil {
