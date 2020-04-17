613d49e550a92af49a0e95265ec6f76d064624f5
Martin Kunc <5441732+martinkunc@users.noreply.github.com>
2020-04-17 15:57:12 +0200

    Refactoring collector, add Doc and doc generator

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/openshift/insights-operator
 go 1.12
 
 require (
-	github.com/coreos/bbolt v1.3.3 // indirect
 	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
 	github.com/getsentry/raven-go v0.2.1-0.20190513200303-c977f96e1095 // indirect
 	github.com/gogo/protobuf v1.3.1 // indirect
--- pkg/gather/clusterconfig/clusterconfig_test.go
+++ pkg/gather/clusterconfig/clusterconfig_test.go
@@ -3,6 +3,7 @@ package clusterconfig
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -85,6 +86,36 @@ func TestConfigMapAnonymizer(t *testing.T) {
 		})
 	}
 
+}
+
+func ExampleGatherMostRecentMetrics_Test() {
+	b, err := ExampleMostRecentMetrics()
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Print(string(b))
+	// Output:
+	// [{"Name":"config/metrics","Captured":"0001-01-01T00:00:00Z","Fingerprint":"","Item":"SGVsbG8sIGNsaWVudAo="}]
+}
+
+func ExampleGatherClusterOperators_Test() {
+	b, err := ExampleClusterOperators()
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Print(string(b))
+	// Output:
+	// [{"Name":"config/clusteroperator/","Captured":"0001-01-01T00:00:00Z","Fingerprint":"","Item":{"metadata":{"creationTimestamp":null},"spec":{},"status":{"conditions":[{"type":"Degraded","status":"","lastTransitionTime":null}],"extension":null}}}]
+}
+
+func ExampleGatherUnhealthyNodes_Test() {
+	b, err := ExampleUnhealthyNodes()
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Print(string(b))
+	// Output:
+	// [{"Name":"config/node/","Captured":"0001-01-01T00:00:00Z","Fingerprint":"","Item":{"metadata":{"creationTimestamp":null},"spec":{},"status":{"conditions":[{"type":"Ready","status":"False","lastHeartbeatTime":null,"lastTransitionTime":null}],"daemonEndpoints":{"kubeletEndpoint":{"Port":0}},"nodeInfo":{"machineID":"","systemUUID":"","bootID":"","kernelVersion":"","osImage":"","containerRuntimeVersion":"","kubeletVersion":"","kubeProxyVersion":"","operatingSystem":"","architecture":""}}}}]
 }
 
 func mustNotFail(t *testing.T, err interface{}, fmtstr string) {
