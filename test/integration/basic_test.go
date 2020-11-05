501cae9f116532d5e88401269c725e8d72d290ae
Pavel Simovec <psimovec@redhat.com>
2020-11-05 12:31:29 +0100

    backport e2e tests 4.5
    Signed-off-by: Pavel Simovec <psimovec@redhat.com>

--- test/integration/basic_test.go
+++ test/integration/basic_test.go
@@ -2,90 +2,170 @@ package integration
 
 import (
 	"encoding/json"
+	"fmt"
 	"testing"
 	"time"
 
+	configv1 "github.com/openshift/api/config/v1"
+	"github.com/openshift/insights-operator/pkg/controller/status"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 )
 
-// Check if opt-in/opt-out works
-func TestOptOutOptIn(t *testing.T) {
-	// Backup pull secret from openshift-config namespace.
-	// oc extract secret/pull-secret -n openshift-config --to=.
-	pullSecret, err := clientset.CoreV1().Secrets("openshift-config").Get("pull-secret", metav1.GetOptions{})
-	if err != nil {
-		t.Fatal(err.Error())
+// TestPullSecretExists makes sure that required pull-secret exists when tests are started
+func TestPullSecretExists(t *testing.T) {
+	pullSecret, err := clientset.CoreV1().Secrets(OpenShiftConfig).Get(PullSecret, metav1.GetOptions{})
+	if errors.IsNotFound(err) {
+		t.Fatalf("The pull-secret should exist when cluster boots up: %s", err)
 	}
-
-	// Edit the `.dockerconfigjson` file that was downloaded.
-	// Remove the `cloud.openshift.com` JSON entry.
-	newPullSecret := pullSecret.DeepCopy()
-	secretConfig := newPullSecret.Data[".dockerconfigjson"]
-
+	if err != nil {
+		t.Fatalf("The pull-secret read failed: %s", err)
+	}
+	var (
+		secretConfig []byte
+		ok           bool
+	)
+	if secretConfig, ok = pullSecret.Data[".dockerconfigjson"]; !ok {
+		t.Fatalf("The pull-secret didn't contain .dockerconfigjson key: %s", err)
+	}
 	obj := map[string]interface{}{}
-	errUnmarshal := json.Unmarshal([]byte(secretConfig), &obj)
+	errUnmarshal := json.Unmarshal(secretConfig, &obj)
 	if errUnmarshal != nil {
 		t.Fatal(errUnmarshal.Error())
 	}
 	creds := obj["auths"].(map[string]interface{})
-	delete(creds, "cloud.openshift.com")
+	if _, ok := creds[CloudOpenShiftCom]; !ok {
+		t.Fatalf("not found secret for cloud.openshift.com")
+	}
+}
+
+func TestIsIOHealthy(t *testing.T) {
+	checkPodsLogs(t, `The operator is healthy`)
+}
+
+// Check if an archive is uploaded and insights results retrieved in a reasonable amount of time
+// This test can be performed on OCP 4.7 and newer
+func TestArchiveUploadedAndResultReceived(t *testing.T) {
+	start := logLineTime(t, `Reporting status periodically to .* every`)
+	end := logLineTime(t, `Successfully reported id=`)
+	uploadingTime := duration(t, start, end)
+	t.Logf("Archive upload time is %v seconds", uploadingTime)
+}
+
+// Check if opt-in/opt-out works
+func TestOptOutOptIn(t *testing.T) {
+	// initially IO should be running
+	errDisabled := wait.PollImmediate(1*time.Second, 30*time.Second, func() (bool, error) {
+		insightsNotDisabled := !operatorStatus(t, clusterOperatorInsights(), status.OperatorDisabled, configv1.ConditionTrue)
+		if insightsNotDisabled {
+			return true, nil
+		}
+		return false, nil
+	})
+	if errDisabled != nil {
+		t.Fatalf("The Cluster Operator wasn't enabled in the beginning")
+	}
+
+	// Backup pull secret from openshift-config namespace.
+	// oc extract secret/pull-secret -n openshift-config --to=.
+	pullSecret, err := clientset.CoreV1().Secrets(OpenShiftConfig).Get(PullSecret, metav1.GetOptions{})
+	if err != nil {
+		t.Fatalf("The pull-secret read failed: %s", err)
+	}
+	// Backup support secret from openshift-config namespace.
+	// oc extract secret/support -n openshift-config --to=.
+	supportSecret, err := clientset.CoreV1().Secrets(OpenShiftConfig).Get(Support, metav1.GetOptions{})
+	if err != nil {
+		t.Fatalf("The pull-secret read failed: %s", err)
+	}
+	resetSecrets := func() {
+		err := forceUpdateSecret(OpenShiftConfig, PullSecret, pullSecret)
+		if err != nil {
+			t.Error(err)
+		}
+		err = forceUpdateSecret(OpenShiftConfig, Support, supportSecret)
+		if err != nil {
+			t.Error(err)
+		}
+	}
+	// Edit the `.dockerconfigjson` file that was downloaded.
+	// Remove the `cloud.openshift.com` JSON entry.
+	var (
+		secretConfig []byte
+		ok           bool
+	)
+	newPullSecret := pullSecret.DeepCopy()
+	if secretConfig, ok = newPullSecret.Data[".dockerconfigjson"]; !ok {
+		t.Fatalf("The pull-secret didn't contain .dockerconfigjson key: %s", err)
+	}
+	obj := map[string]interface{}{}
+	errUnmarshal := json.Unmarshal(secretConfig, &obj)
+	if errUnmarshal != nil {
+		t.Fatal(errUnmarshal.Error())
+	}
+	creds := obj["auths"].(map[string]interface{})
+
+	delete(creds, CloudOpenShiftCom)
 
 	modifiedConfig, err := json.Marshal(obj)
 	if err != nil {
 		t.Fatal(err.Error())
 	}
-
 	newPullSecret.Data[".dockerconfigjson"] = modifiedConfig
 
 	// Update the global cluster pull secret.
 	// oc set data secret/pull-secret -n openshift-config --from-file=.dockerconfigjson=<pull-secret-location>
-	_, err = clientset.CoreV1().Secrets("openshift-config").Update(newPullSecret)
+	_, err = clientset.CoreV1().Secrets(OpenShiftConfig).Update(newPullSecret)
 	if err != nil {
-		t.Fatal(err.Error())
+		t.Fatalf("Cannot update pull-secret with secret without cloud.redhat.com secret. Error: %s ", err)
 	}
-	// Check the logs -  Logs contains the line "The operator is marked as disabled" and no reports are uploaded
+	newSupportSecret := supportSecret.DeepCopy()
+	// set the upload interval to 1 to speed up test
+	newSupportSecret.Data["interval"] = []byte(fmt.Sprintf("%s", time.Minute*1))
+
+	// Update the global cluster pull secret.
+	// oc set data secret/support -n openshift-config --from-file=interval=<pull-secret-location>
+	_, err = clientset.CoreV1().Secrets(OpenShiftConfig).Update(newSupportSecret)
+	if err != nil {
+		t.Fatalf("Cannot update support secret with secret with short interval. Error: %s ", err)
+	}
+	defer func() {
+		resetSecrets()
+	}()
+
+	// Check the ClusterOperator status - Status is updated even while operator is only initializing.
+	// Disabled status is written to logs only after initializing period
 	restartInsightsOperator(t)
-	checkPodsLogs(t, clientset, "The operator is marked as disabled")
 
-	// Upload backuped secret
-	latestSecret, err := clientset.CoreV1().Secrets("openshift-config").Get("pull-secret", metav1.GetOptions{})
-	if err != nil {
-		t.Fatal(err.Error())
-	}
-	resourceVersion := latestSecret.GetResourceVersion()
-	pullSecret.SetResourceVersion(resourceVersion) // need to update the version, otherwise operation is not permitted
-
-	errConfig := wait.PollImmediate(5*time.Second, 10*time.Minute, func() (bool, error) {
-		objs := map[string]interface{}{}
-		errUnmarshals := json.Unmarshal([]byte(pullSecret.Data[".dockerconfigjson"]), &objs)
-		if errUnmarshals != nil {
-			t.Fatal(errUnmarshal.Error())
-		}
-		for key := range objs["auths"].(map[string]interface{}) {
-			if key == "cloud.openshift.com" {
-				return true, nil
-			}
+	// Wait for operator to become disabled because of removed pull-secret
+	errDisabled = wait.PollImmediate(1*time.Second, 30*time.Second, func() (bool, error) {
+		insightsDisabled := operatorConditionCheck(t, clusterOperatorInsights(), "Disabled")
+		if insightsDisabled {
+			return true, nil
 		}
 		return false, nil
 	})
-	t.Log(errConfig)
-
-	newSecret, err := clientset.CoreV1().Secrets("openshift-config").Update(pullSecret)
-	if err != nil {
-		t.Fatal(err.Error())
+	if errDisabled != nil {
+		t.Fatalf("The Cluster Operator wasn't disabled after removing pull-secret")
 	}
-	t.Logf("%v\n", newSecret)
+	// Return to original pull secret, so that IO would be enabled again
+	err = forceUpdateSecret(OpenShiftConfig, PullSecret, pullSecret)
+	if err != nil {
+		t.Errorf("cannot return original pull-secret: %s", err)
+	}
 
 	// Check if reports are uploaded - Logs show that insights-operator is enabled and reports are uploaded
 	restartInsightsOperator(t)
-	errDisabled := wait.PollImmediate(1*time.Second, 20*time.Minute, func() (bool, error) {
-		insightsDisabled := isOperatorDisabled(t, clusterOperatorInsights())
-		if insightsDisabled {
-			return false, nil
+	errDisabled = wait.PollImmediate(1*time.Second, 3*time.Minute, func() (bool, error) {
+		insightsNotDisabled := !operatorStatus(t, clusterOperatorInsights(), status.OperatorDisabled, configv1.ConditionTrue)
+		if insightsNotDisabled {
+			return true, nil
 		}
-		return true, nil
+		return false, nil
 	})
-	t.Log(errDisabled)
-	checkPodsLogs(t, clientset, "Successfully reported")
+	if errDisabled != nil {
+		t.Fatalf("The Cluster Operator wasn't enabled after setting original pull-secret")
+	}
+	checkPodsLogs(t, "Successfully reported")
 }
--- test/integration/main_test.go
+++ test/integration/main_test.go
@@ -5,23 +5,44 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"regexp"
 	"strings"
 	"testing"
 	"time"
 
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/tools/remotecommand"
+
 	configv1 "github.com/openshift/api/config/v1"
 	configv1client "github.com/openshift/client-go/config/clientset/versioned/typed/config/v1"
 	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
+	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+)
+
+const (
+	// namespace openshift-config
+	OpenShiftConfig = "openshift-config"
+
+	// secret support form namespace openshift-config
+	Support = "support"
+
+	// secret pull-secret from namespace openshift-config
+	PullSecret = "pull-secret"
+
+	// secret support pull token key is under auth
+	CloudOpenShiftCom = "cloud.openshift.com"
 )
 
 var clientset = kubeClient()
 var configClient = configV1Client()
 
-func kubeClient() (result *kubernetes.Clientset) {
+func kubeconfig() (config *restclient.Config) {
 	kubeconfig, ok := os.LookupEnv("KUBECONFIG") // variable is a path to the local kubeconfig
 	if !ok {
 		fmt.Printf("kubeconfig variable is not set\n")
@@ -34,8 +55,11 @@ func kubeClient() (result *kubernetes.Clientset) {
 		fmt.Printf("%#v", err)
 		os.Exit(1)
 	}
+	return config
+}
 
-	clientset, err := kubernetes.NewForConfig(config)
+func kubeClient() (result *kubernetes.Clientset) {
+	clientset, err := kubernetes.NewForConfig(kubeconfig())
 	if err != nil {
 		panic(err.Error())
 	}
@@ -43,76 +67,71 @@ func kubeClient() (result *kubernetes.Clientset) {
 }
 
 func configV1Client() (result *configv1client.ConfigV1Client) {
-	kubeconfig, ok := os.LookupEnv("KUBECONFIG") // variable is a path to the local kubeconfig
-	if !ok {
-		fmt.Printf("kubeconfig variable is not set\n")
-	} else {
-		fmt.Printf("KUBECONFIG=%s\n", kubeconfig)
-	}
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		fmt.Printf("%#v", err)
-		os.Exit(1)
-	}
-
-	client, err := configv1client.NewForConfig(config)
+	client, err := configv1client.NewForConfig(kubeconfig())
 	if err != nil {
 		panic(err.Error())
 	}
 	return client
 }
 
-func clusterOperatorInsights() *configv1.ClusterOperator {
-	// get info about insights cluster operator
-	operator, err := configClient.ClusterOperators().Get("insights", metav1.GetOptions{})
+func clusterOperator(clusterName string) *configv1.ClusterOperator {
+	// get info about given cluster operator
+	operator, err := configClient.ClusterOperators().Get(clusterName, metav1.GetOptions{})
 	if err != nil {
+		// TODO -> change to t.Fatal in follow-up PR
 		panic(err.Error())
 	}
 	return operator
 }
 
-func isOperatorDegraded(t *testing.T, operator *configv1.ClusterOperator) bool {
-	statusConditions := operator.Status.Conditions
-
-	for _, condition := range statusConditions {
-		if condition.Type == "Degraded" {
-			if condition.Status != "True" {
-				t.Log("Operator is not degraded")
-				return false
-			}
-		}
-	}
-	t.Log("Operator is degraded")
-	return true
+func clusterOperatorInsights() *configv1.ClusterOperator {
+	// TODO -> delete this function in follow-up PR
+	return clusterOperator("insights")
 }
 
-func isOperatorDisabled(t *testing.T, operator *configv1.ClusterOperator) bool {
+func operatorConditionCheck(t *testing.T, operator *configv1.ClusterOperator, conditionType configv1.ClusterStatusConditionType) bool {
 	statusConditions := operator.Status.Conditions
 
 	for _, condition := range statusConditions {
-		if condition.Type == "Disabled" {
-			if condition.Status != "True" {
-				t.Log("Operator is not Disabled")
-				return false
+		if (conditionType == condition.Type) && (condition.Status == "True") {
+			t.Logf("%s Operator is %v", time.Now(), conditionType)
+			return true
+		}
+	}
+	t.Logf("%s Operator is not %v", time.Now(), conditionType)
+	return false
+}
+
+func operatorStatus(t *testing.T, operator *configv1.ClusterOperator, conditionType configv1.ClusterStatusConditionType, conditionStatus configv1.ConditionStatus) bool {
+	statusConditions := operator.Status.Conditions
+
+	for _, condition := range statusConditions {
+		if condition.Type == conditionType {
+			if condition.Status == conditionStatus {
+				t.Logf("Operator has Condition Type %s with status %s", conditionType, conditionStatus)
+				return true
 			}
 		}
 	}
-	t.Log("Operator is disabled")
-	return true
+	t.Logf("Operator doesn't have Condition Type %s with status %s", conditionType, conditionStatus)
+	return false
 }
 
 func restartInsightsOperator(t *testing.T) {
+	deleteAllPods(t, "openshift-insights")
+}
+
+func deleteAllPods(t *testing.T, namespace string) {
 	// restart insights-operator (delete pods)
-	pods, err := clientset.CoreV1().Pods("openshift-insights").List(metav1.ListOptions{})
+	pods, err := clientset.CoreV1().Pods(namespace).List(metav1.ListOptions{})
 	if err != nil {
 		t.Fatal(err.Error())
 	}
 
 	for _, pod := range pods.Items {
-		clientset.CoreV1().Pods("openshift-insights").Delete(pod.Name, &metav1.DeleteOptions{})
-		err := wait.PollImmediate(1*time.Second, 20*time.Minute, func() (bool, error) {
-			_, err := clientset.CoreV1().Pods("openshift-insights").Get(pod.Name, metav1.GetOptions{})
+		clientset.CoreV1().Pods(namespace).Delete(pod.Name, &metav1.DeleteOptions{})
+		err := wait.PollImmediate(1*time.Second, 1*time.Minute, func() (bool, error) {
+			_, err := clientset.CoreV1().Pods(namespace).Get(pod.Name, metav1.GetOptions{})
 			if err == nil {
 				t.Logf("the pod is not yet deleted: %v\n", err)
 				return false, nil
@@ -124,15 +143,15 @@ func restartInsightsOperator(t *testing.T) {
 	}
 
 	// check new pods are created and running
-	errPod := wait.PollImmediate(1*time.Second, 10*time.Minute, func() (bool, error) {
-		newPods, _ := clientset.CoreV1().Pods("openshift-insights").List(metav1.ListOptions{})
+	errPod := wait.PollImmediate(1*time.Second, 1*time.Minute, func() (bool, error) {
+		newPods, _ := clientset.CoreV1().Pods(namespace).List(metav1.ListOptions{})
 		if len(newPods.Items) == 0 {
 			t.Log("pods are not yet created")
 			return false, nil
 		}
 
 		for _, newPod := range newPods.Items {
-			pod, err := clientset.CoreV1().Pods("openshift-insights").Get(newPod.Name, metav1.GetOptions{})
+			pod, err := clientset.CoreV1().Pods(namespace).Get(newPod.Name, metav1.GetOptions{})
 			if err != nil {
 				panic(err.Error())
 			}
@@ -147,47 +166,212 @@ func restartInsightsOperator(t *testing.T) {
 	t.Log(errPod)
 }
 
-func checkPodsLogs(t *testing.T, kubeClient *kubernetes.Clientset, message string) {
-	newPods, err := kubeClient.CoreV1().Pods("openshift-insights").List(metav1.ListOptions{})
+func logLineTime(t *testing.T, pattern string) time.Time {
+	startOfLine := `^\S\d{2}\d{2}\s\d{2}:\d{2}:\d{2}\.\d{6}\s*\d+\s\S+\.go:\d+]\s`
+	lc := LogChecker(t).Timeout(10).Search(startOfLine + pattern)
+	if lc.Err != nil {
+		t.Fatalf("Couldn't find \"%s\"", pattern)
+	}
+	str := strings.Split(strings.Split(lc.Result, ".")[0], " ")[1]
+	time1, err := time.Parse("15:04:05", str)
+	e(t, err, "time parsing fail")
+	return time1
+}
+
+func duration(t *testing.T, start time.Time, end time.Time) float64 {
+	difference := end.Sub(start).Seconds()
+	if difference < 0 {
+		difference = 24*time.Hour.Seconds() + difference
+	}
+	return difference
+}
+
+func LogChecker(t *testing.T) *LogCheck {
+	defaults := &LogCheck{
+		interval:   time.Second,
+		logOptions: corev1.PodLogOptions{},
+		timeout:    5 * time.Minute,
+		failFast:   true,
+		test:       t,
+		clientset:  clientset,
+	}
+	return defaults
+}
+
+func checkPodsLogs(t *testing.T, message string) *LogCheck {
+	return LogChecker(t).Search(message)
+}
+
+func forceUpdateSecret(ns string, secretName string, secret *v1.Secret) error {
+	latestSecret, err := clientset.CoreV1().Secrets(ns).Get(secretName, metav1.GetOptions{})
+	if err != nil && !errors.IsNotFound(err) {
+		return fmt.Errorf("cannot read the original secret: %s", err)
+	}
+	if errors.IsNotFound(err) {
+		// new objects shouldn't have resourceVersion set
+		secret.SetResourceVersion("")
+		_, err = clientset.CoreV1().Secrets(ns).Create(secret)
+		if err != nil {
+			return fmt.Errorf("cannot create the original secret: %s", err)
+		}
+		return nil
+	}
+	resourceVersion := latestSecret.GetResourceVersion()
+	secret.SetUID(latestSecret.GetUID())
+	secret.SetResourceVersion(resourceVersion) // need to update the version, otherwise operation is not permitted
+
+	_, err = clientset.CoreV1().Secrets("openshift-config").Update(secret)
+	if err != nil {
+		return fmt.Errorf("Unable to update original secret: %s", err)
+	}
+	return nil
+}
+
+func ExecCmd(t *testing.T, client kubernetes.Interface, podName string, namespace string,
+	command string, stdin io.Reader) (string, string, error) {
+	cmd := []string{
+		"/bin/bash",
+		"-c",
+		command,
+	}
+	req := client.CoreV1().RESTClient().Post().Resource("pods").Name(podName).
+		Namespace(namespace).SubResource("exec")
+	option := &corev1.PodExecOptions{
+		Command: cmd,
+		Stdin:   true,
+		Stdout:  true,
+		Stderr:  true,
+		TTY:     false,
+	}
+	if stdin == nil {
+		option.Stdin = false
+	}
+	req.VersionedParams(
+		option,
+		scheme.ParameterCodec,
+	)
+	exec, err := remotecommand.NewSPDYExecutor(kubeconfig(), "POST", req.URL())
+	if err != nil {
+		return "", "", err
+	}
+	var stdout, stderr bytes.Buffer
+	err = exec.Stream(remotecommand.StreamOptions{
+		Stdin:  stdin,
+		Stdout: &stdout,
+		Stderr: &stderr,
+	})
+	if err != nil {
+		return "", "", err
+	}
+
+	return stdout.String(), stderr.String(), nil
+}
+
+func e(t *testing.T, err error, message string) {
+	if err != nil {
+		t.Fatal(message, err.Error())
+	}
+}
+
+func findPod(t *testing.T, kubeClient *kubernetes.Clientset, namespace string, prefix string) *corev1.Pod {
+	newPods, err := kubeClient.CoreV1().Pods(namespace).List(metav1.ListOptions{})
 	if err != nil {
 		t.Fatal(err.Error())
 	}
-
 	for _, newPod := range newPods.Items {
-		pod, err := kubeClient.CoreV1().Pods("openshift-insights").Get(newPod.Name, metav1.GetOptions{})
-		if err != nil {
-			panic(err.Error())
-		}
-
-		errLog := wait.PollImmediate(5*time.Second, 30*time.Minute, func() (bool, error) {
-			req := kubeClient.CoreV1().Pods(pod.Namespace).GetLogs(pod.Name, &corev1.PodLogOptions{})
-			podLogs, err := req.Stream()
-			if err != nil {
-				return false, nil
-			}
-			defer podLogs.Close()
-
-			buf := new(bytes.Buffer)
-			_, err = io.Copy(buf, podLogs)
-			if err != nil {
-				panic(err.Error())
-			}
-			log := buf.String()
-
-			result := strings.Contains(log, message)
-			if result == false {
-				t.Logf("No %s in logs\n", message)
-				t.Logf("Logs for verification: ****\n%s", log)
-				return false, nil
-			}
-
-			t.Logf("%s found\n", message)
-			return true, nil
-		})
-		if errLog != nil {
-			t.Error(errLog)
+		if strings.HasPrefix(newPod.Name, prefix) {
+			return &newPod
 		}
 	}
+	return nil
+}
+
+func degradeOperatorMonitoring(t *testing.T) func() {
+	// delete just in case it was already there, so we don't care about error
+	pod := findPod(t, clientset, "openshift-monitoring", "cluster-monitoring-operator")
+	clientset.CoreV1().ConfigMaps(pod.Namespace).Delete("cluster-monitoring-config", &metav1.DeleteOptions{})
+	operatorConditionCheck(t, clusterOperator("monitoring"), "Degraded")
+	_, err := clientset.CoreV1().ConfigMaps(pod.Namespace).Create(
+		&corev1.ConfigMap{ObjectMeta: metav1.ObjectMeta{Name: "cluster-monitoring-config"}, Data: map[string]string{"config.yaml": "telemeterClient: enabled: NOT_BOOELAN"}},
+	)
+	e(t, err, "Failed to create ConfigMap")
+	err = clientset.CoreV1().Pods(pod.Namespace).Delete(pod.Name, &metav1.DeleteOptions{})
+	e(t, err, "Failed to delete Pod")
+	wait.PollImmediate(1*time.Second, 5*time.Minute, func() (bool, error) {
+		return operatorConditionCheck(t, clusterOperator("monitoring"), "Degraded"), nil
+	})
+	return func() {
+		clientset.CoreV1().ConfigMaps(pod.Namespace).Delete("cluster-monitoring-config", &metav1.DeleteOptions{})
+		wait.PollImmediate(3*time.Second, 3*time.Minute, func() (bool, error) {
+			insightsDegraded := operatorConditionCheck(t, clusterOperator("monitoring"), "Degraded")
+			return !insightsDegraded, nil
+		})
+	}
+}
+
+func changeReportTimeInterval(t *testing.T, newInterval []byte) []byte {
+	supportSecret, _ := clientset.CoreV1().Secrets(OpenShiftConfig).Get(Support, metav1.GetOptions{})
+	previousInterval := supportSecret.Data["interval"]
+	supportSecret.Data["interval"] = newInterval
+	err := forceUpdateSecret(OpenShiftConfig, Support, supportSecret)
+	e(t, err, "changing report time interval failed")
+	restartInsightsOperator(t)
+	t.Log("forcing update secret")
+	return previousInterval
+}
+
+func ChangeReportTimeInterval(t *testing.T, minutes time.Duration) func() {
+	previousInterval := changeReportTimeInterval(t, []byte(fmt.Sprintf("%s", time.Minute*minutes)))
+	return func() { changeReportTimeInterval(t, previousInterval) }
+}
+
+func latestArchiveFiles(t *testing.T) []string {
+	insightsPod := findPod(t, clientset, "openshift-insights", "insights-operator")
+	archiveLogFiles := `tar tf $(ls -dtr /var/lib/insights-operator/* | tail -1)`
+	stdout, _, _ := ExecCmd(t, clientset, insightsPod.Name, "openshift-insights", archiveLogFiles, nil)
+	stdout = strings.TrimSpace(stdout)
+	return strings.Split(stdout, "\n")
+}
+
+func allFilesMatch(t *testing.T, prettyName string, files []string, regex *regexp.Regexp) error {
+	for _, file := range files {
+		if !regex.MatchString(file) {
+			t.Errorf(`%s file "%s" does not match pattern "%s"`, prettyName, file, regex.String())
+		}
+	}
+	return nil
+}
+
+func matchingFileExists(t *testing.T, prettyName string, files []string, regex *regexp.Regexp) error {
+	count := 0
+	for _, file := range files {
+		if regex.MatchString(file) {
+			count++
+		}
+	}
+
+	word := "files"
+	suffix := ""
+	if count == 1 {
+		word = "file"
+		suffix = "es"
+	}
+	t.Logf("%d %s %s match%s pattern `%s`", count, prettyName, word, suffix, regex.String())
+
+	if count != 0 {
+		return nil
+	}
+	return fmt.Errorf("did not find any (%s)file matching %s", prettyName, regex.String())
+}
+
+func latestArchiveCheckFiles(t *testing.T, prettyName string, check func(*testing.T, string, []string, *regexp.Regexp) error, pattern string) error {
+	latestFiles := latestArchiveFiles(t)
+	if len(latestFiles) == 0 {
+		t.Fatal("No files in archive to check")
+	}
+	regex, err := regexp.Compile(pattern)
+	e(t, err, "failed to compile pattern")
+	return check(t, prettyName, latestFiles, regex)
 }
 
 func TestMain(m *testing.M) {
@@ -203,7 +387,7 @@ func TestMain(m *testing.M) {
 func waitForOperator(kubeClient *kubernetes.Clientset) error {
 	depClient := kubeClient.AppsV1().Deployments("openshift-insights")
 
-	err := wait.PollImmediate(1*time.Second, 20*time.Minute, func() (bool, error) {
+	err := wait.PollImmediate(1*time.Second, 1*time.Minute, func() (bool, error) {
 		_, err := depClient.Get("insights-operator", metav1.GetOptions{})
 		if err != nil {
 			fmt.Printf("error waiting for operator deployment to exist: %v\n", err)
