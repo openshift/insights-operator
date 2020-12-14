cb81bdca45547e4e5bff2ee80a7b9f1fbe764eea
Serhii Zakharov <szakharo@redhat.com>
2020-12-14 16:17:09 +0100

    gathering config maps from kube-system namespace

--- pkg/gather/clusterconfig/config_maps.go
+++ pkg/gather/clusterconfig/config_maps.go
@@ -5,18 +5,20 @@ import (
 	"encoding/base64"
 	"encoding/pem"
 	"fmt"
+	"regexp"
 	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	_ "k8s.io/apimachinery/pkg/runtime/serializer/yaml"
 	"k8s.io/client-go/kubernetes"
 	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
-
-	_ "k8s.io/apimachinery/pkg/runtime/serializer/yaml"
 
 	"github.com/openshift/insights-operator/pkg/record"
 )
 
-// GatherConfigMaps fetches the ConfigMaps from namespace openshift-config.
+// GatherConfigMaps fetches the ConfigMaps from the following namespaces:
+//   - openshift-config.
+//   - kube-system
 //
 // Anonymization: If the content of ConfigMap contains a parseable PEM structure (like certificate) it removes the inside of PEM blocks.
 // For ConfigMap of type BinaryData it is encoded as standard base64.
@@ -33,12 +35,28 @@ func GatherConfigMaps(g *Gatherer) func() ([]record.Record, []error) {
 		if err != nil {
 			return nil, []error{err}
 		}
-		return gatherConfigMaps(g.ctx, gatherKubeClient.CoreV1())
+		return gatherConfigMaps(g.ctx, gatherKubeClient.CoreV1(), []string{
+			"openshift-config",
+			"kube-system",
+		})
 	}
 }
 
-func gatherConfigMaps(ctx context.Context, coreClient corev1client.CoreV1Interface) ([]record.Record, []error) {
-	cms, err := coreClient.ConfigMaps("openshift-config").List(ctx, metav1.ListOptions{})
+func gatherConfigMaps(ctx context.Context, coreClient corev1client.CoreV1Interface, namespaces []string) ([]record.Record, []error) {
+	var allRecords []record.Record
+	var allErrors []error
+
+	for _, namespace := range namespaces {
+		records, errors := gatherConfigMapsForNamespace(ctx, coreClient, namespace)
+		allRecords = append(allRecords, records...)
+		allErrors = append(allErrors, errors...)
+	}
+
+	return allRecords, allErrors
+}
+
+func gatherConfigMapsForNamespace(ctx context.Context, coreClient corev1client.CoreV1Interface, namespace string) ([]record.Record, []error) {
+	cms, err := coreClient.ConfigMaps(namespace).List(ctx, metav1.ListOptions{})
 	if err != nil {
 		return nil, []error{err}
 	}
@@ -62,7 +80,7 @@ func gatherConfigMaps(ctx context.Context, coreClient corev1client.CoreV1Interfa
 }
 
 // ConfigMapAnonymizer implements serialization of configmap
-// and potentially anonymizes if it is a certificate
+// and potentially anonymizes if it is a certificate or an ssh public key
 type ConfigMapAnonymizer struct {
 	v            []byte
 	encodeBase64 bool
@@ -73,7 +91,7 @@ func (a ConfigMapAnonymizer) Marshal(_ context.Context) ([]byte, error) {
 	c := []byte(anonymizeConfigMap(a.v))
 	if a.encodeBase64 {
 		buff := make([]byte, base64.StdEncoding.EncodedLen(len(c)))
-		base64.StdEncoding.Encode(buff, []byte(c))
+		base64.StdEncoding.Encode(buff, c)
 		c = buff
 	}
 	return c, nil
@@ -85,6 +103,9 @@ func (a ConfigMapAnonymizer) GetExtension() string {
 }
 
 func anonymizeConfigMap(dv []byte) string {
+	sshRegex := regexp.MustCompile(`ssh-rsa\s+[A-Za-z0-9+/=]+\s+.*`)
+	dv = sshRegex.ReplaceAll(dv, []byte("ssh-rsa ANONYMIZED"))
+
 	anonymizedPemBlock := `-----BEGIN CERTIFICATE-----
 ANONYMIZED
 -----END CERTIFICATE-----
