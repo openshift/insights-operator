8d47b655d2e21138b07ea24fd2aa07a74580c9a2
W. Trevor King <wking@tremily.us>
2021-03-23 16:47:42 -0700

    pkg/gather/clusterconfig: Collapse GatherClusterID into GatherClusterVersion
    There's no reason to fetch the ClusterVersion twice, even if we are
    creating two Records based on its content.  This also sets the stage
    for gathering additional items like sad cluster-version operator pods.
    
    I personally don't see a need to call out the fact that ClusterVersion
    included clusterID, but Tomas wanted it [1].
    
    [1]: https://github.com/openshift/insights-operator/pull/381#discussion_r601169083

--- pkg/gather/clusterconfig/version.go
+++ pkg/gather/clusterconfig/version.go
@@ -5,7 +5,6 @@ import (
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/rest"
 
 	configv1 "github.com/openshift/api/config/v1"
 	configv1client "github.com/openshift/client-go/config/clientset/versioned/typed/config/v1"
@@ -15,59 +14,45 @@ import (
 	"github.com/openshift/insights-operator/pkg/utils/marshal"
 )
 
-// GatherClusterVersion fetches the ClusterVersion - the ClusterVersion with name version.
+// GatherClusterVersion fetches the ClusterVersion (including the cluster ID) with the name 'version' and its resources.
 //
 // The Kubernetes api https://github.com/openshift/client-go/blob/master/config/clientset/versioned/typed/config/v1/clusterversion.go#L50
 // Response see https://docs.openshift.com/container-platform/4.3/rest_api/index.html#clusterversion-v1config-openshift-io
 //
-// Location in archive: config/version/
-// See: docs/insights-archive-sample/config/version
-// Id in config: version
+// * Location in archive: config/version/
+// * See: docs/insights-archive-sample/config/version
+// * Location of cluster ID: config/id
+// * See: docs/insights-archive-sample/config/id
+// * Id in config: version
 func GatherClusterVersion(g *Gatherer, c chan<- gatherResult) {
 	defer close(c)
-	config, err := getClusterVersion(g.ctx, g.gatherKubeConfig)
+	gatherConfigClient, err := configv1client.NewForConfig(g.gatherKubeConfig)
 	if err != nil {
 		c <- gatherResult{nil, []error{err}}
 		return
 	}
-	c <- gatherResult{[]record.Record{{Name: "config/version", Item: record.JSONMarshaller{Object: anonymizeClusterVersion(config)}}}, nil}
+	records, errors := getClusterVersion(g.ctx, gatherConfigClient)
+	c <- gatherResult{records, errors}
 }
 
-func getClusterVersion(ctx context.Context, kubeConfig *rest.Config) (*configv1.ClusterVersion, error) {
-	gatherConfigClient, err := configv1client.NewForConfig(kubeConfig)
-	if err != nil {
-		return nil, err
-	}
-	config, err := gatherConfigClient.ClusterVersions().Get(ctx, "version", metav1.GetOptions{})
+func getClusterVersion(ctx context.Context, configClient configv1client.ConfigV1Interface) ([]record.Record, []error) {
+	config, err := configClient.ClusterVersions().Get(ctx, "version", metav1.GetOptions{})
 	if errors.IsNotFound(err) {
 		return nil, nil
 	}
 	if err != nil {
-		return nil, err
+		return nil, []error{err}
 	}
-	return config, nil
-}
 
-// GatherClusterID stores ClusterID from ClusterVersion version
-// This method uses data already collected by Get ClusterVersion. In particular field .Spec.ClusterID
-// The Kubernetes api https://github.com/openshift/client-go/blob/master/config/clientset/versioned/typed/config/v1/clusterversion.go#L50
-// Response see https://github.com/openshift/api/blob/master/config/v1/types_cluster_version.go#L38
-//
-// * Location in archive: config/id/
-// * See: docs/insights-archive-sample/config/id
-// * Id in config: id
-func GatherClusterID(g *Gatherer, c chan<- gatherResult) {
-	defer close(c)
-	version, err := getClusterVersion(g.ctx, g.gatherKubeConfig)
-	if err != nil {
-		c <- gatherResult{nil, []error{err}}
-		return
+	records := []record.Record{
+		{Name: "config/version", Item: record.JSONMarshaller{Object: anonymizeClusterVersion(config)}},
 	}
-	if version == nil {
-		c <- gatherResult{nil, nil}
-		return
+
+	if config.Spec.ClusterID != "" {
+		records = append(records, record.Record{Name: "config/id", Item: marshal.Raw{Str: string(config.Spec.ClusterID)}})
 	}
-	c <- gatherResult{[]record.Record{{Name: "config/id", Item: marshal.Raw{Str: string(version.Spec.ClusterID)}}}, nil}
+
+	return records, nil
 }
 
 func anonymizeClusterVersion(version *configv1.ClusterVersion) *configv1.ClusterVersion {
