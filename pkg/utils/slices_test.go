0916877bb8fc5fa4a748d14ade2709b9129141df
Serhii Zakharov <szakharo@redhat.com>
2021-04-07 13:28:59 +0200

    tests for slices

---
+++ pkg/utils/slices_test.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_SortAndRemoveDuplicates(t *testing.T) {
+	testIntSlice := func(original []int, expected []int) {
+		got := append([]int{}, original...) // clone it
+		SortAndRemoveDuplicates(&got, func(i, j int) bool {
+			return got[i] < got[j]
+		})
+		assert.Equal(t, expected, got)
+	}
+
+	testIntSlice([]int{1, 9, 5, -3, 9, -3, -3, 5, 2, 11}, []int{-3, 1, 2, 5, 9, 11})
+	testIntSlice([]int{1, 2, 3, 4, 5}, []int{1, 2, 3, 4, 5})
+	testIntSlice([]int{5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5})
+	testIntSlice([]int{1, 2, 2, 3, 3, 3}, []int{1, 2, 3})
+	testIntSlice([]int{3, 3, 3, 2, 2, 1, 2, 2, 3, 3, 3}, []int{1, 2, 3})
+	testIntSlice([]int{1}, []int{1})
+	testIntSlice([]int{}, []int{})
+
+	testStringSlice := func(original []string, expected []string) {
+		got := append([]string{}, original...) // clone it
+		SortAndRemoveDuplicates(&got, func(i, j int) bool {
+			return got[i] < got[j]
+		})
+		assert.Equal(t, expected, got)
+	}
+
+	testStringSlice([]string{"abc", "acd", "ade"}, []string{"abc", "acd", "ade"})
+	testStringSlice([]string{"acd", "ade", "abc"}, []string{"abc", "acd", "ade"})
+}
+
+func Test_TakeLastNItemsFromByteArray(t *testing.T) {
+	assert.Equal(t, []byte{0, 0, 1, 2, 3, 4, 5}, TakeLastNItemsFromByteArray([]byte{1, 2, 3, 4, 5}, 7))
+	assert.Equal(t, []byte{1, 2, 3, 4, 5}, TakeLastNItemsFromByteArray([]byte{1, 2, 3, 4, 5}, 5))
+	assert.Equal(t, []byte{4, 5}, TakeLastNItemsFromByteArray([]byte{1, 2, 3, 4, 5}, 2))
+	assert.Equal(t, []byte{}, TakeLastNItemsFromByteArray([]byte{1, 2, 3, 4, 5}, 0))
+	assert.Equal(t, []byte{0, 0, 0, 0}, TakeLastNItemsFromByteArray([]byte{0, 0, 0}, 4))
+}
