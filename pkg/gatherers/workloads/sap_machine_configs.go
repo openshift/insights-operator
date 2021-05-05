63e62b6d89f0bfec9395483b5b2d08e8a1313ddb
Ivo Meixner <natiwastaken@gmail.com>
2021-05-05 06:51:43 +0200

    Add filtering of gathered machineconfigs Also added error dump to log for debugging.

--- pkg/gatherers/workloads/sap_machine_configs.go
+++ pkg/gatherers/workloads/sap_machine_configs.go
@@ -8,6 +8,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/klog/v2"
 
 	"github.com/openshift/insights-operator/pkg/record"
 )
@@ -27,25 +28,31 @@ func gatherSAPMachineConfig(ctx context.Context, dynamicClient dynamic.Interface
 	gvrMC := schema.GroupVersionResource{Group: "machineconfiguration.openshift.io", Version: "v1", Resource: "machineconfigs"}
 	// gvrMCP := schema.GroupVersionResource{Group: "machineconfiguration.openshift.io", Version: "v1", Resource: "machineconfigpools"}
 	mcList, err := dynamicClient.Resource(gvrMC).List(ctx, metav1.ListOptions{})
+	klog.Warningf("------------------>>>>>> %#v", err)
 	if errors.IsNotFound(err) {
 		return nil, nil
 	}
 	if err != nil {
 		return nil, []error{err}
 	}
-	// klog.Warningf("%#v\n%#v", mcList, err)
 
-	records := []record.Record{
-		{
-			Name: "config/machineconfigs/aaaaaa",
-			Item: record.JSONMarshaller{Object: mcList},
-		},
-	}
+	records := []record.Record{}
 	for _, mc := range mcList.Items {
-		records = append(records, record.Record{
-			Name: fmt.Sprintf("config/machineconfigs/%s", mc.GetName()),
-			Item: record.JSONMarshaller{Object: mc.Object},
-		})
+		shouldBeGathered := mc.GetName() == "75-worker-sap-data-intelligence"
+
+		for _, ownerRef := range mc.GetOwnerReferences() {
+			if ownerRef.Kind == "ContainerRuntimeConfig" && ownerRef.Name == "sdi-pids-limit" {
+				shouldBeGathered = true
+				break
+			}
+		}
+
+		if shouldBeGathered {
+			records = append(records, record.Record{
+				Name: fmt.Sprintf("config/machineconfigs/%s", mc.GetName()),
+				Item: record.JSONMarshaller{Object: mc.Object},
+			})
+		}
 	}
 
 	return records, nil
