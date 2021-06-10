022b8fd0489d2daa4cffd131443e7f89f6516a39
Serhii Zakharov <szakharo@redhat.com>
2021-06-10 10:25:50 +0200

    calm down linters

--- pkg/gatherers/common/constants.go
+++ pkg/gatherers/common/constants.go
@@ -1,6 +1,8 @@
 package common
 
 const (
-	ImageConfigQPS   = 10
+	// ImageConfigQPS is the maximum qps for image client
+	ImageConfigQPS = 10
+	// ImageConfigBurst is the maximum burst for image client
 	ImageConfigBurst = 10
 )
