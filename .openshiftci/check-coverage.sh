362e093467c38919b0cc1606f3076d7fc56e38d2
Serhii Zakharov <szakharo@redhat.com>
2021-07-01 20:10:27 +0200

    moved the script to .openshiftci

--- .openshiftci/check-coverage.sh
+++ .openshiftci/check-coverage.sh
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 THRESHOLD=50
 
 RED_BG=$(tput setab 1)
@@ -21,21 +20,17 @@ GREEN_BG=$(tput setab 2)
 NC=$(tput sgr0) # No Color
 
 if ! [[ $* == *do-not-run-tests* ]]; then
-	make test || exit 1
+    make test || exit 1
 fi
 
 go_tool_cover_output=$(go tool cover -func=cover.out)
 
-if [[ $* == *verbose* ]]; then
-    echo "$go_tool_cover_output"
-fi
+echo "$go_tool_cover_output"
 
-if (( $(echo "$go_tool_cover_output" | tail -n 1 | awk '{print $NF}' | grep -E "^[0-9]+" -o) >= THRESHOLD )); then
-    echo "$go_tool_cover_output"
+if (($(echo "$go_tool_cover_output" | tail -n 1 | awk '{print $NF}' | grep -E "^[0-9]+" -o) >= THRESHOLD)); then
     echo -e "${GREEN_BG}[OK]${NC} Code coverage is OK"
     exit 0
 else
-    echo "$go_tool_cover_output"
     echo -e "${RED_BG}[FAIL]${NC} Code coverage have to be at least $THRESHOLD%"
     exit 1
 fi
