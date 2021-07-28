01e103f1f3d652b249c6bb82aee829877cb6719f
Ricardo Lüders <rluders@redhat.com>
2021-07-29 01:35:20 +0200

    Bug 1986833: Gather Openshift Logging Stack Data (#476)
    * Gather Openshift Logging information
    
    Removing extra line
    
    fix error if clusterloggings resource is missing
    
    * add: openshift_logging_test
    
    * update: gather documentation
    
    * add: sample data
    
    * clean: remove unused coreclient
    
    * fix: code review
    
    * fix: replace marshaller
    
    * Improve logging documentation
    
    this adds the API reference to the openshift logging gather.
    
    * Update sample data for openshift logging
    
    * Adds openshift logging unit test
    
    this PR adds a real test to openshift logging stack gather.
    
    * Make lint happy
    
    * fix: sa permissions
    
    * fix: test strings
    
    * fix: documentation
    
    * fix: document path

---
+++ pkg/gatherers/clusterconfig/openshift_logging.go
@@ -0,0 +1,52 @@
+package clusterconfig
+
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/klog/v2"
+
+	"github.com/openshift/insights-operator/pkg/record"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/dynamic"
+)
+
+// GatherOpenshiftLogging collects `logging.openshift.io` resources
+// from Openshift Logging Stack.
+//
+// API Reference:
+//   https://github.com/openshift/cluster-logging-operator/blob/master/pkg/apis/logging/v1/clusterlogging_types.go
+//
+// * Location in archive: config/logging/<namespace>/<name>.json
+// * Since versions:
+//   * 4.9+
+func (g *Gatherer) GatherOpenshiftLogging(ctx context.Context) ([]record.Record, []error) {
+	gatherDynamicClient, err := dynamic.NewForConfig(g.gatherKubeConfig)
+	if err != nil {
+		return nil, []error{err}
+	}
+
+	return gatherOpenshiftLogging(ctx, gatherDynamicClient)
+}
+
+func gatherOpenshiftLogging(ctx context.Context, dynamicClient dynamic.Interface) ([]record.Record, []error) {
+	loggingResourceList, err := dynamicClient.Resource(openshiftLoggingResource).List(ctx, metav1.ListOptions{})
+	if errors.IsNotFound(err) {
+		return nil, nil
+	}
+	if err != nil {
+		klog.V(2).Infof("Unable to list %s resource due to: %s", openshiftLoggingResource, err)
+		return nil, []error{err}
+	}
+
+	var records []record.Record
+	for i := range loggingResourceList.Items {
+		item := loggingResourceList.Items[i]
+		records = append(records, record.Record{
+			Name: fmt.Sprintf("config/logging/%s/%s", item.GetNamespace(), item.GetName()),
+			Item: record.ResourceMarshaller{Resource: &item},
+		})
+	}
+	return records, nil
+}
---
+++ pkg/gatherers/clusterconfig/openshift_logging_test.go
@@ -0,0 +1,84 @@
+package clusterconfig
+
+import (
+	"context"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+	"k8s.io/client-go/dynamic"
+	dynamicfake "k8s.io/client-go/dynamic/fake"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func Test_GatherOpenshiftLogging(t *testing.T) {
+	var openshiftLoggingYAML = `
+apiVersion: logging.openshift.io/v1
+kind: ClusterLogging
+metadata:
+    name: instance 
+    namespace: openshift-logging
+`
+	type args struct {
+		ctx           context.Context
+		dynamicClient dynamic.Interface
+	}
+
+	tests := []struct {
+		name          string
+		args          args
+		totalRecords  int
+		recordName    string
+		expectedError error
+	}{
+		{
+			name: "check for clusterlogging resource",
+			args: args{
+				ctx: context.TODO(),
+				dynamicClient: dynamicfake.NewSimpleDynamicClientWithCustomListKinds(runtime.NewScheme(), map[schema.GroupVersionResource]string{
+					openshiftLoggingResource: "ClusterLoggingList",
+				}),
+			},
+			totalRecords:  1,
+			recordName:    "config/logging/openshift-logging/instance",
+			expectedError: nil,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			decUnstructured := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+			testOpenshiftLoggingResource := &unstructured.Unstructured{}
+
+			_, _, err := decUnstructured.Decode([]byte(openshiftLoggingYAML), nil, testOpenshiftLoggingResource)
+			if err != nil {
+				t.Fatal("unable to decode clusterlogging ", err)
+			}
+			_, err = tt.args.dynamicClient.
+				Resource(openshiftLoggingResource).
+				Namespace("openshift-logging").
+				Create(context.Background(), testOpenshiftLoggingResource, metav1.CreateOptions{})
+			if err != nil {
+				t.Fatalf("unable to create fake resource %s", err)
+			}
+
+			records, errs := gatherOpenshiftLogging(tt.args.ctx, tt.args.dynamicClient)
+			if len(errs) > 0 {
+				if errs[0].Error() != tt.expectedError.Error() {
+					t.Fatalf("unexpected errors: %v", errs[0].Error())
+				}
+			}
+			if len(records) != tt.totalRecords {
+				t.Errorf("gatherOpenshiftLogging() got = %v, want %v", len(records), tt.totalRecords)
+			}
+			if records[0].Name != tt.recordName {
+				t.Errorf("gatherOpenshiftLogging() name = %v, want %v ", records[0].Name, tt.recordName)
+			}
+		})
+	}
+}
