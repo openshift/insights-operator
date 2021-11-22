d2bc5ae4fdbf5188eacce96e01b4abbe4ec80c66
Ivo Meixner <natiwastaken@gmail.com>
2021-11-22 09:58:08 +0100

    Add simple test for ReadAllLinesWithPrefix

---
+++ pkg/utils/read_lines_with_prefix_test.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"io"
+	"strings"
+	"testing"
+)
+
+func Test_ReadAllLinesWithPrefix(t *testing.T) {
+	reader := strings.NewReader(strings.Join([]string{
+		"prefix_first_line",
+		"second_line",
+		"third_line",
+		"prefix_fourth_line",
+		"prefix_last_line",
+	}, string(MetricsLineSep)))
+	lines, err := ReadAllLinesWithPrefix(reader, []byte("prefix_"))
+	if err != nil && err != io.EOF {
+		t.Fatal(err)
+	}
+	if string(lines) != "prefix_first_line\nprefix_fourth_line\nprefix_last_line" {
+		t.Fatalf("unexpected lines returned: %q", lines)
+	}
+}
