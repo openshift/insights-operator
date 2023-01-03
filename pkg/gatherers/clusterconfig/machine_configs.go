95079f35547b3d37280750cb81bbd2dc423165eb
Ricardo Lüders <rluders@redhat.com>
2023-01-03 17:22:04 +0100

    chore(docs): clusterconfig/machine_configs updated

--- pkg/gatherers/clusterconfig/machine_configs.go
+++ pkg/gatherers/clusterconfig/machine_configs.go
@@ -13,17 +13,32 @@ import (
 	"github.com/openshift/insights-operator/pkg/record"
 )
 
-// GatherMachineConfigs collects MachineConfigs definitions. Following data is intentionally removed from the definitions:
-// * `spec.config.storage.files`
-// * `spec.config.passwd.users`
+// GatherMachineConfigs Collects MachineConfigs definitions. Following data is intentionally removed from the definitions:
+// - `spec.config.storage.files`
+// - `spec.config.passwd.users`
 //
-// Response see https://docs.openshift.com/container-platform/4.7/rest_api/machine_apis/machineconfig-machineconfiguration-openshift-io-v1.html
+// ### API Reference
+// - https://docs.openshift.com/container-platform/4.7/rest_api/machine_apis/machineconfig-machineconfiguration-openshift-io-v1.html
 //
-// * Location in archive: config/machineconfigs/{name}.json
-// * Id in config: clusterconfig/machine_configs
-// * Since versions:
-//   - 4.8.5+
-//   - 4.9+
+// ### Sample data
+// - docs/insights-archive-sample/config/machineconfigs/75-worker-sap-data-intelligence.json
+//
+// ### Location in archive
+// | Version   | Path														|
+// | --------- | --------------------------------------------------------	|
+// | >= 4.8.2  | config/machineconfigs/{resource}.json 							|
+//
+// ### Config ID
+// `clusterconfig/machine_configs`
+//
+// ### Released version
+// - 4.8.2
+//
+// ### Backported versions
+// None
+//
+// ### Notes
+// None
 func (g *Gatherer) GatherMachineConfigs(ctx context.Context) ([]record.Record, []error) {
 	gatherDynamicClient, err := dynamic.NewForConfig(g.gatherKubeConfig)
 	if err != nil {
