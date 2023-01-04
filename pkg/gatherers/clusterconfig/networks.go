0c045b71e200029a8c3cbdd2a379461878b27e6a
Ricardo Lüders <rluders@redhat.com>
2023-01-04 22:26:35 +0100

    chore(docs): clusterconfig/networks updated

--- pkg/gatherers/clusterconfig/networks.go
+++ pkg/gatherers/clusterconfig/networks.go
@@ -11,14 +11,31 @@ import (
 	"github.com/openshift/insights-operator/pkg/record"
 )
 
-// GatherClusterNetwork fetches the cluster Network - the Network with name cluster.
+// GatherClusterNetwork Collects the cluster Network - the Network with name cluster.
 //
-// The Kubernetes api https://github.com/openshift/client-go/blob/master/config/clientset/versioned/typed/config/v1/network.go#L50
-// Response see https://docs.openshift.com/container-platform/4.3/rest_api/index.html#network-v1-config-openshift-io
+// ### API Reference
+// - https://github.com/openshift/client-go/blob/master/config/clientset/versioned/typed/config/v1/network.go#L50
+// - https://docs.openshift.com/container-platform/4.3/rest_api/index.html#network-v1-config-openshift-io
 //
-// * Location in archive: config/network/
-// * See: docs/insights-archive-sample/config/network
-// * Id in config: clusterconfig/networks
+// ### Sample data
+// - docs/insights-archive-sample/config/.json
+//
+// ### Location in archive
+// | Version   | Path														|
+// | --------- | --------------------------------------------------------	|
+// | >= 4.2    | config/network.json 					                    |
+//
+// ### Config ID
+// `clusterconfig/networks`
+//
+// ### Released version
+// - 4.2
+//
+// ### Backported versions
+// None
+//
+// ### Notes
+// None
 func (g *Gatherer) GatherClusterNetwork(ctx context.Context) ([]record.Record, []error) {
 	gatherConfigClient, err := configv1client.NewForConfig(g.gatherKubeConfig)
 	if err != nil {
