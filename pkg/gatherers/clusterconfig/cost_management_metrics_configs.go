49d5f38444c95b896cfe3b83d9e89ee914df757e
Ricardo Lüders <rluders@redhat.com>
2023-01-05 17:36:26 +0100

    chore(docs): clusterconfig/cost_management_metrics_configs updated

--- pkg/gatherers/clusterconfig/cost_management_metrics_configs.go
+++ pkg/gatherers/clusterconfig/cost_management_metrics_configs.go
@@ -11,13 +11,31 @@ import (
 	"github.com/openshift/insights-operator/pkg/record"
 )
 
-// GatherCostManagementMetricsConfigs collects CostManagementMetricsConfigs definitions.
-// * Location in archive: config/cost_management_metrics_configs/{name}.json
-// * Id in config: clusterconfig/cost_management_metrics_configs
-// * Since versions:
-//   - 4.8.27+
-//   - 4.9.13+
-//   - 4.10+
+// GatherCostManagementMetricsConfigs Collects CostManagementMetricsConfigs definitions.
+//
+// ### API Reference
+// None
+//
+// ### Sample data
+// - docs/insights-archive-sample/config/cost_management_metrics_configs
+//
+// ### Location in archive
+// | Version   | Path														|
+// | --------- | --------------------------------------------------------	|
+// | >= 4.10   | config/cost_management_metrics_configs/{name}.json 		|
+//
+// ### Config ID
+// `clusterconfig/cost_management_metrics_configs`
+//
+// ### Released version
+// - 4.10
+//
+// ### Backported versions
+// - 4.8.27+
+// - 4.9.13+
+//
+// ### Notes
+// None
 func (g *Gatherer) GatherCostManagementMetricsConfigs(ctx context.Context) ([]record.Record, []error) {
 	gatherDynamicClient, err := dynamic.NewForConfig(g.gatherKubeConfig)
 	if err != nil {
