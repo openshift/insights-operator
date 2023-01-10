ae6341a3d9a82db552922931cbd3bdcb90b844b4
Ricardo Lüders <rluders@redhat.com>
2023-01-10 19:19:51 +0100

    chore(docs): clusterconfig/openshift_sdn_controller_logs updated

--- pkg/gatherers/clusterconfig/openshift_sdn_controller_logs.go
+++ pkg/gatherers/clusterconfig/openshift_sdn_controller_logs.go
@@ -10,31 +10,43 @@ import (
 	"github.com/openshift/insights-operator/pkg/record"
 )
 
-// GatherOpenshiftSDNControllerLogs collects logs from sdn-controller pod in openshift-sdn namespace with following substrings:
-//   - "Node %s is not Ready": A node has been set offline for egress IPs because it is reported not ready at API
-//   - "Node %s may be offline... retrying": An egress node has failed the egress IP health check once,
-//     so it has big chances to be marked as offline soon or, at the very least, there has been a connectivity glitch.
-//   - "Node %s is offline": An egress node has failed enough probes to have been marked offline for egress IPs.
-//     If it has egress CIDRs assigned, its egress IPs have been moved to other nodes.
-//     Indicates issues at either the node or the network between the master and the node.
-//   - "Node %s is back online": This indicates that a node has recovered from the condition described
-//     at the previous message, by starting succeeding the egress IP health checks.
-//     Useful just in case that previous “Node %s is offline” messages are lost,
-//     so that we have a clue that there was failure previously.
+// GatherOpenshiftSDNControllerLogs Collects logs from sdn-controller pod in openshift-sdn namespace with
+// following substrings:
 //
-// The Kubernetes API:
+// - "Node %s is not Ready": A node has been set offline for egress IPs because it is reported not ready at API
+// - "Node %s may be offline... retrying": An egress node has failed the egress IP health check once,
+// so it has big chances to be marked as offline soon or, at the very least, there has been a connectivity glitch.
+// - "Node %s is offline": An egress node has failed enough probes to have been marked offline for egress IPs.
+// If it has egress CIDRs assigned, its egress IPs have been moved to other nodes. Indicate issues at either the node
+// or the network between the master and the node.
+// - "Node %s is back online": This indicates that a node has recovered from the condition described
+// at the previous message, by starting succeeding the egress IP health checks. Useful just in case that previous
+// “Node %s is offline” messages are lost, so that we have a clue that there was failure previously.
 //
-//	https://github.com/kubernetes/client-go/blob/master/kubernetes/typed/core/v1/pod_expansion.go#L48
+// ### API Reference
+// - https://github.com/kubernetes/client-go/blob/master/kubernetes/typed/core/v1/pod_expansion.go#L48
+// - https://docs.openshift.com/container-platform/4.6/rest_api/workloads_apis/pod-core-v1.html#apiv1namespacesnamespacepodsnamelog
 //
-// Response see:
+// ### Sample data
+// - docs/insights-archive-sample/config/pod/openshift-sdn/logs/sdn-f2694/errors.log
+// - docs/insights-archive-sample/config/pod/openshift-sdn/logs/sdn-controller-l8gq9/errors.log
 //
-//	https://docs.openshift.com/container-platform/4.6/rest_api/workloads_apis/pod-core-v1.html#apiv1namespacesnamespacepodsnamelog
+// ### Location in archive
+// | Version   | Path														|
+// | --------- | --------------------------------------------------------	|
+// | >= 4.7.0  | config/pod/openshift-sdn/logs/{pod-name}/errors.log		|
 //
-// * Location in archive: config/pod/openshift-sdn/logs/{pod-name}/errors.log
-// * Id in config: clusterconfig/openshift_sdn_controller_logs
-// * Since versions:
-//   - 4.6.21+
-//   - 4.7+
+// ### Config ID
+// `clusterconfig/`
+//
+// ### Released version
+// - 4.7.0
+//
+// ### Backported versions
+// - 4.6.21+
+//
+// ### Notes
+// None
 func (g *Gatherer) GatherOpenshiftSDNControllerLogs(ctx context.Context) ([]record.Record, []error) {
 	containersFilter := common.LogContainersFilter{
 		Namespace:     "openshift-sdn",
