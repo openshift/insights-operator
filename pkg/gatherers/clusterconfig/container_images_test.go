73296b0238ff7bc6ca1ab8a5800086b0d6e730b5
Isaac Jimeno <ijimeno@redhat.com>
2023-01-26 13:44:59 +0100

    Add unit test for env var obfuscation on container images gatherer

--- pkg/gatherers/clusterconfig/container_images_test.go
+++ pkg/gatherers/clusterconfig/container_images_test.go
@@ -135,3 +135,42 @@ func Test_ContainerImages_Gather(t *testing.T) { //nolint: funlen,gocyclo
 		}
 	}
 }
+
+func Test_ContainerImages_EnvVar_Obfuscation(t *testing.T) {
+	// Given
+	mock := []corev1.Container{
+		{
+			Env: []corev1.EnvVar{
+				{Name: "NO_TARGET", Value: "original_value"},
+				{Name: "HTTP_PROXY", Value: "original_value"},
+				{Name: "HTTPS_PROXY", Value: "original_value"},
+			},
+		},
+	}
+
+	// When
+	obfuscateContainerEnvVars(mock)
+
+	// Assert
+	t.Run("Non target env vars keep their original value", func(t *testing.T) {
+		test := mock[0].Env[0]
+		if test.Value != "original_value" {
+			t.Logf("\nexpected the variable '%s' to have 'original_value'\ngot: %s", test.Name, test.Value)
+			t.Fail()
+		}
+	})
+	t.Run("HTTP_PROXY is updated with obfuscated value", func(t *testing.T) {
+		test := mock[0].Env[1]
+		if test.Value == "original_value" {
+			t.Logf("\nexpected the variable '%s' to have 'xxxxx' value\ngot: %s", test.Name, test.Value)
+			t.Fail()
+		}
+	})
+	t.Run("HTTPS_PROXY is updated with obfuscated value", func(t *testing.T) {
+		test := mock[0].Env[2]
+		if test.Value == "original_value" {
+			t.Logf("\nexpected the variable '%s' to have 'xxxxx' value\ngot: %s", test.Name, test.Value)
+			t.Fail()
+		}
+	})
+}
