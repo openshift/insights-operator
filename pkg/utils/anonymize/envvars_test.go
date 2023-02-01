f9126b404bafb1df606711b02aba53422bf1415d
Isaac Jimeno <ijimeno@redhat.com>
2023-02-01 10:12:16 +0100

    Use assert library

--- pkg/utils/anonymize/envvars_test.go
+++ pkg/utils/anonymize/envvars_test.go
@@ -3,6 +3,7 @@ package anonymize
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 )
 
@@ -25,23 +26,14 @@ func Test_EnvVar_Obfuscation(t *testing.T) {
 	// Assert
 	t.Run("Non target env vars keep their original value", func(t *testing.T) {
 		test := mock[0].Env[0]
-		if test.Value != envOriginalValue {
-			t.Logf("\nexpected the variable '%s' to have 'original_value'\ngot: %s", test.Name, test.Value)
-			t.Fail()
-		}
+		assert.Equal(t, envOriginalValue, test.Value)
 	})
 	t.Run("HTTP_PROXY is updated with obfuscated value", func(t *testing.T) {
 		test := mock[0].Env[1]
-		if test.Value == envOriginalValue {
-			t.Logf("\nexpected the variable '%s' to have 'xxxxxxxxxxxxxx' value\ngot: %s", test.Name, test.Value)
-			t.Fail()
-		}
+		assert.NotEqual(t, envOriginalValue, test.Value)
 	})
 	t.Run("HTTPS_PROXY is updated with obfuscated value", func(t *testing.T) {
 		test := mock[0].Env[2]
-		if test.Value == envOriginalValue {
-			t.Logf("\nexpected the variable '%s' to have 'xxxxxxxxxxxxxx' value\ngot: %s", test.Name, test.Value)
-			t.Fail()
-		}
+		assert.NotEqual(t, envOriginalValue, test.Value)
 	})
 }
