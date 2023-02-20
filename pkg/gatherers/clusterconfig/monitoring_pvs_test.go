b810cd7039525b0e1966274d86af43df1c1e062a
Isaac Jimeno <ijimeno@redhat.com>
2023-02-20 18:11:42 +0100

    WIP Rework PV Gatherer simple unit test

--- pkg/gatherers/clusterconfig/monitoring_pvs_test.go
+++ pkg/gatherers/clusterconfig/monitoring_pvs_test.go
@@ -4,69 +4,38 @@ import (
 	"context"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	kubefake "k8s.io/client-go/kubernetes/fake"
 )
 
 func Test_GatherMonitoring(t *testing.T) {
 
-	coreClient := kubefake.NewSimpleClientset().CoreV1()
-	ctx := context.Background()
+	t.Run("Existent Persistent Volume within the namespace is gathered", func(t *testing.T) {
+		// Given
+		mockPVC := &corev1.PersistentVolumeClaim{
+			ObjectMeta: metav1.ObjectMeta{Name: "testName", Namespace: "testNamespace"},
+			Spec:       corev1.PersistentVolumeClaimSpec{VolumeName: "mockPVName"},
+		}
 
-	coreClient.Namespaces().Create(ctx, &corev1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test",
-			Namespace: "test",
-		},
-	}, metav1.CreateOptions{})
-
-	claim, err := coreClient.PersistentVolumeClaims("test").Create(
-		ctx,
-		&corev1.PersistentVolumeClaim{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "test",
-				Namespace: "test",
-			},
-			Spec: corev1.PersistentVolumeClaimSpec{
-				VolumeName: "amanda",
-			},
-		},
-		metav1.CreateOptions{},
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	t.Logf("claim: %v\n", claim)
-
-	volume, err := coreClient.PersistentVolumes().Create(
-		ctx,
-		&corev1.PersistentVolume{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "test",
-			},
-			Status: corev1.PersistentVolumeStatus{
-				Phase: "Available",
-			},
+		mockPV := &corev1.PersistentVolume{
+			ObjectMeta: metav1.ObjectMeta{Name: "mockPVName"},
+			Status:     corev1.PersistentVolumeStatus{Phase: "Available"},
 			Spec: corev1.PersistentVolumeSpec{
 				PersistentVolumeSource: corev1.PersistentVolumeSource{},
+				StorageClassName:       "testStorageClass",
 			},
-		},
-		metav1.CreateOptions{},
-	)
-	if err != nil {
-		t.Fatal(err)
-	}
+		}
+		coreclient := kubefake.NewSimpleClientset([]runtime.Object{mockPVC, mockPV}...)
 
-	t.Logf("volume: %v\n", volume)
+		// When
+		records, errors := gatherPVsByNamespace(context.TODO(), coreclient.CoreV1(), "testNamespace")
 
-	list, err := coreClient.PersistentVolumeClaims("test").List(ctx, metav1.ListOptions{})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	t.Logf("list: %v\n", list)
-
-	coreClient.PersistentVolumes().Get(ctx, "", metav1.ListOptions{})
+		// Assert
+		assert.Len(t, errors, 0)
+		assert.Len(t, records, 1)
+		assert.Equal(t, "config/pod/testNamespace/mockPVName", records[0].Name)
+	})
 }
