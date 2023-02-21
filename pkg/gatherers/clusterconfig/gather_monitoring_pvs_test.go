84903ab3ee427284aa8f5d2f9437826ecac20258
Isaac Jimeno <ijimeno@redhat.com>
2023-02-21 18:56:42 +0100

    Rework unit tests to table format and Add two more tests

--- pkg/gatherers/clusterconfig/gather_monitoring_pvs_test.go
+++ pkg/gatherers/clusterconfig/gather_monitoring_pvs_test.go
@@ -12,30 +12,70 @@ import (
 )
 
 func Test_GatherMonitoring(t *testing.T) {
-
-	t.Run("Existent Persistent Volume within the namespace is gathered", func(t *testing.T) {
-		// Given
-		mockPVC := &corev1.PersistentVolumeClaim{
-			ObjectMeta: metav1.ObjectMeta{Name: "testName", Namespace: "testNamespace"},
-			Spec:       corev1.PersistentVolumeClaimSpec{VolumeName: "mockPVName"},
-		}
-
-		mockPV := &corev1.PersistentVolume{
-			ObjectMeta: metav1.ObjectMeta{Name: "mockPVName"},
-			Status:     corev1.PersistentVolumeStatus{Phase: "Available"},
-			Spec: corev1.PersistentVolumeSpec{
-				PersistentVolumeSource: corev1.PersistentVolumeSource{},
-				StorageClassName:       "testStorageClass",
+	testCases := []struct {
+		name               string
+		pvc                *corev1.PersistentVolumeClaim
+		pv                 *corev1.PersistentVolume
+		assertErrorsNumber int
+		assertRecordNumber int
+		assertRecord       bool
+		assertError        bool
+	}{
+		{
+			name: "Existent Persistent Volume within the namespace is gathered",
+			pvc: &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{Name: "mockName", Namespace: "mockNamespace"},
+				Spec:       corev1.PersistentVolumeClaimSpec{VolumeName: "test"},
 			},
-		}
-		coreclient := kubefake.NewSimpleClientset([]runtime.Object{mockPVC, mockPV}...)
+			pv: &corev1.PersistentVolume{
+				ObjectMeta: metav1.ObjectMeta{Name: "test"},
+				Status:     corev1.PersistentVolumeStatus{Phase: "Available"},
+				Spec: corev1.PersistentVolumeSpec{
+					PersistentVolumeSource: corev1.PersistentVolumeSource{},
+				},
+			},
+			assertErrorsNumber: 0,
+			assertRecordNumber: 1,
+			assertRecord:       true,
+		},
+		{
+			name: "Non-existent Persistent Volume within the namespace throws an error",
+			pvc: &corev1.PersistentVolumeClaim{
+				ObjectMeta: metav1.ObjectMeta{Name: "mockName", Namespace: "mockNamespace"},
+				Spec:       corev1.PersistentVolumeClaimSpec{VolumeName: "test"},
+			},
+			pv:                 &corev1.PersistentVolume{},
+			assertErrorsNumber: 1,
+			assertRecordNumber: 0,
+			assertError:        true,
+		},
+		{
+			name:               "Non-existent Persistent Volume Claim does not throw any error",
+			pvc:                &corev1.PersistentVolumeClaim{},
+			pv:                 &corev1.PersistentVolume{},
+			assertErrorsNumber: 0,
+			assertRecordNumber: 0,
+		},
+	}
 
-		// When
-		records, errors := gatherPVsByNamespace(context.TODO(), coreclient.CoreV1(), "testNamespace")
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			// Given
+			coreclient := kubefake.NewSimpleClientset([]runtime.Object{testCase.pvc, testCase.pv}...)
 
-		// Assert
-		assert.Len(t, errors, 0)
-		assert.Len(t, records, 1)
-		assert.Equal(t, "config/pod/testNamespace/mockPVName", records[0].Name)
-	})
+			// When
+			records, errors := gatherPVsByNamespace(context.TODO(), coreclient.CoreV1(), "mockNamespace")
+
+			// Assert
+			assert.Len(t, records, testCase.assertRecordNumber)
+			if testCase.assertRecord {
+				assert.Equal(t, "config/persistentvolumes/test", records[0].Name)
+			}
+
+			assert.Len(t, errors, testCase.assertErrorsNumber)
+			if testCase.assertError {
+				assert.ErrorContains(t, errors[0], "not found")
+			}
+		})
+	}
 }
