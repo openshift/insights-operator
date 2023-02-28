5b465b98f65db7d0a8a2300a85ad16bd30a7a60f
Richard Hrmo <61187052+rhrmo@users.noreply.github.com>
2023-02-28 11:36:55 +0100

    test(oauth): add unit test for oauth.go and rename it to gather_cluster_oauth.go  (#738)
    * rename oauth.go and create unit tests for it
    
    * lint fix
    
    * refactoring

---
+++ pkg/gatherers/clusterconfig/gather_cluster_oauth_test.go
@@ -0,0 +1,50 @@
+package clusterconfig
+
+import (
+	"context"
+	"testing"
+
+	configv1 "github.com/openshift/api/config/v1"
+	configfake "github.com/openshift/client-go/config/clientset/versioned/fake"
+	"github.com/openshift/insights-operator/pkg/record"
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func Test_gatherClusterOAuth(t *testing.T) {
+	tests := []struct {
+		name            string
+		oAuthDefinition *configv1.OAuth
+		wantRecords     []record.Record
+		wantErrCount    int
+	}{
+		{
+			name:            "successful retrieval cluster oauth",
+			oAuthDefinition: &configv1.OAuth{ObjectMeta: metav1.ObjectMeta{Name: "cluster"}},
+			wantRecords: []record.Record{
+				{
+					Name: "config/oauth",
+					Item: record.ResourceMarshaller{
+						Resource: &configv1.OAuth{ObjectMeta: metav1.ObjectMeta{Name: "cluster"}},
+					},
+				},
+			},
+			wantErrCount: 0,
+		},
+		{
+			name:            "empty oauth",
+			oAuthDefinition: &configv1.OAuth{},
+			wantRecords:     nil,
+			wantErrCount:    0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			configClient := configfake.NewSimpleClientset(tt.oAuthDefinition)
+			records, errs := gatherClusterOAuth(context.TODO(), configClient.ConfigV1())
+			assert.Equal(t, tt.wantRecords, records)
+			assert.Len(t, errs, tt.wantErrCount)
+		})
+	}
+}
