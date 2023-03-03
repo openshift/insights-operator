a561c5825bf5d9a81a33a220b8a415e9969d97eb
Isaac Jimeno <ijimeno@redhat.com>
2023-03-03 18:30:09 +0100

    Add new unit tests for monitoring gatherer private logic

--- pkg/gatherers/clusterconfig/gather_monitoring_pvs_test.go
+++ pkg/gatherers/clusterconfig/gather_monitoring_pvs_test.go
@@ -11,7 +11,7 @@ import (
 	kubefake "k8s.io/client-go/kubernetes/fake"
 )
 
-func Test_GatherMonitoring(t *testing.T) {
+func Test_GatherMonitoring_gather(t *testing.T) {
 	testCases := []struct {
 		name               string
 		pvc                *corev1.PersistentVolumeClaim
@@ -93,6 +93,126 @@ func Test_GatherMonitoring(t *testing.T) {
 			if testCase.assertError {
 				assert.ErrorContains(t, errors[0], "not found")
 			}
+		})
+	}
+}
+
+func Test_GatherMonitoring_unmarshalDefaultPath(t *testing.T) {
+	testCases := []struct {
+		name     string
+		yamlMock string
+		expected string
+		wantErr  bool
+		errMsg   string
+	}{
+		{
+			name:     "Trying to unmarshal expected data returns prometheus name",
+			yamlMock: "prometheusK8s:\n  volumeClaimTemplate:\n    metadata:\n      name: mock\n    spec:\n      storageClassName: local-storage\n      resources:\n        requests:\n          storage: 2Gi\n",
+			expected: "mock",
+			wantErr:  false,
+		},
+		{
+			name:     "Trying to unmarshal unexpected data returns an error",
+			yamlMock: "prometheusK8s:\n  volumeClaimTemplate:\n    spec:\n      storageClassName: local-storage\n      resources:\n        requests:\n          storage: 2Gi\n",
+			wantErr:  true,
+			errMsg:   "can't find prometheusK8s.volumeClaimTemplate.metadata.name",
+		},
+		{
+			name:     "Trying to unmarshal malformed yaml returns an error",
+			yamlMock: "prometheusK8s:\n  volumeClaimTemplate:  dd: dfs:\n",
+			wantErr:  true,
+			errMsg:   "error converting YAML to JSON",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			// Given
+			data := testCase.yamlMock
+			coreclient := kubefake.NewSimpleClientset()
+			gatherer := MonitoringPVGatherer{client: coreclient.CoreV1()}
+
+			// When
+			test, err := gatherer.unmarshalDefaultPath(data)
+
+			// Assert
+			if testCase.wantErr {
+				assert.Error(t, err)
+				assert.ErrorContains(t, err, testCase.errMsg)
+
+			} else {
+				assert.Equal(t, testCase.expected, test)
+				assert.NoError(t, err)
+			}
+
+		})
+	}
+}
+
+func Test_GatherMonitoring_getDefaultPrometheusName(t *testing.T) {
+	testCases := []struct {
+		name     string
+		yamlMock string
+		expected string
+		wantErr  bool
+		errMsg   string
+		cm       *corev1.ConfigMap
+	}{
+		{
+			name:    "No cluster-monitoring-config configmap is defined returns an error",
+			cm:      &corev1.ConfigMap{},
+			wantErr: true,
+			errMsg:  "configmaps \"cluster-monitoring-config\" not found",
+		},
+		{
+			name: "ConfigMap having not config.yaml data entry returns an error",
+			cm: &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{Name: "cluster-monitoring-config", Namespace: "openshift-monitoring"},
+			},
+			wantErr: true,
+			errMsg:  "no config.yaml data on cluster-monitoring-config ConfigMap",
+		},
+		{
+			name: "ConfigMap with valid data returns prometheus name",
+			cm: &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{Name: "cluster-monitoring-config", Namespace: "openshift-monitoring"},
+				Data: map[string]string{
+					"config.yaml": "prometheusK8s:\n  volumeClaimTemplate:\n    metadata:\n      name: mock\n    spec:\n      storageClassName: local-storage\n      resources:\n        requests:\n          storage: 2Gi\n",
+				},
+			},
+			expected: "mock",
+		},
+		{
+			name: "ConfigMap with unvalid data returns an error",
+			cm: &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{Name: "cluster-monitoring-config", Namespace: "openshift-monitoring"},
+				Data: map[string]string{
+					"config.yaml": "otherdata:\n  ",
+				},
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			// Given
+			coreclient := kubefake.NewSimpleClientset([]runtime.Object{testCase.cm}...)
+			gatherer := MonitoringPVGatherer{client: coreclient.CoreV1()}
+
+			// When
+			test, err := gatherer.getDefaultPrometheusName(context.TODO())
+
+			// Assert
+			if testCase.wantErr {
+				assert.Error(t, err)
+				assert.ErrorContains(t, err, testCase.errMsg)
+
+			} else {
+				assert.Equal(t, testCase.expected, test)
+				assert.NoError(t, err)
+			}
+
 		})
 	}
 }
