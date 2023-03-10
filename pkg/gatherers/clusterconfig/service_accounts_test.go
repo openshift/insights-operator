40ad2bbc4723a6ca62cbb59404c3906ca983eb18
Richard Hrmo <61187052+rhrmo@users.noreply.github.com>
2023-03-10 13:01:03 +0100

    OCPBUGS-8082: service_accounts.go Marshal fix (#744)
    * fixed code in service_accounts.go
    
    * updated unit tests for service accounts gathering
    
    * updated archive sample
    
    * Updated code based on review
    
    * updated code based on review and tremes input
    
    * updated code and unit tests based on review

--- pkg/gatherers/clusterconfig/service_accounts_test.go
+++ pkg/gatherers/clusterconfig/service_accounts_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubefake "k8s.io/client-go/kubernetes/fake"
@@ -11,38 +12,90 @@ import (
 
 func Test_ServiceAccounts_Gather(t *testing.T) {
 	tests := []struct {
-		name string
-		data []*corev1.ServiceAccount
-		exp  string
+		name            string
+		namespaces      []string
+		serviceAccounts []*corev1.ServiceAccount
+		exp             string
 	}{
 		{
-			name: "one account",
-			data: []*corev1.ServiceAccount{{
+			name:       "one account",
+			namespaces: []string{"default"},
+			serviceAccounts: []*corev1.ServiceAccount{{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "local-storage-operator",
 					Namespace: "default",
 				},
 				Secrets: []corev1.ObjectReference{{}},
 			}},
-			exp: `{"serviceAccounts":{"TOTAL_COUNT":1,"namespaces":{"default":{"name":"local-storage-operator","secrets":1}}}}`,
+			exp: `{"serviceAccounts":{"TOTAL_COUNT":1,"namespaces":{"default":[{"name":"local-storage-operator","secrets":1}]}}}`,
 		},
 		{
-			name: "multiple accounts",
-			data: []*corev1.ServiceAccount{{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "deployer",
-					Namespace: "openshift",
+			name:       "multiple accounts",
+			namespaces: []string{"openshift", "openshift-apiserver"},
+			serviceAccounts: []*corev1.ServiceAccount{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "deployer",
+						Namespace: "openshift",
+					},
+					Secrets: []corev1.ObjectReference{{}},
 				},
-				Secrets: []corev1.ObjectReference{{}},
-			},
 				{
 					ObjectMeta: metav1.ObjectMeta{
 						Name:      "openshift-apiserver-sa",
 						Namespace: "openshift-apiserver",
 					},
 					Secrets: []corev1.ObjectReference{{}},
-				}},
-			exp: `{"serviceAccounts":{"TOTAL_COUNT":2,"namespaces":{"openshift":{"name":"deployer","secrets":1},"openshift-apiserver":{"name":"openshift-apiserver-sa","secrets":1}}}}`, // nolint: lll
+				},
+			},
+			exp: `{"serviceAccounts":{"TOTAL_COUNT":2,"namespaces":{"openshift":[{"name":"deployer","secrets":1}],"openshift-apiserver":[{"name":"openshift-apiserver-sa","secrets":1}]}}}`, // nolint: lll
+		},
+		{
+			name:       "multiple accounts on the same namespace",
+			namespaces: []string{"openshift"},
+			serviceAccounts: []*corev1.ServiceAccount{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "deployer",
+						Namespace: "openshift",
+					},
+					Secrets: []corev1.ObjectReference{{}},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "builder",
+						Namespace: "openshift",
+					},
+					Secrets: []corev1.ObjectReference{{}},
+				},
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "default",
+						Namespace: "openshift",
+					},
+					Secrets: []corev1.ObjectReference{{}},
+				},
+			},
+			exp: `{"serviceAccounts":{"TOTAL_COUNT":3,"namespaces":{"openshift":[{"name":"builder","secrets":1},{"name":"default","secrets":1},{"name":"deployer","secrets":1}]}}}`, // nolint: lll
+		},
+		{
+			name:       "multiple secrets",
+			namespaces: []string{"default"},
+			serviceAccounts: []*corev1.ServiceAccount{{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "local-storage-operator",
+					Namespace: "default",
+				},
+				Secrets: []corev1.ObjectReference{
+					{
+						Name: "secret1",
+					},
+					{
+						Name: "secret2",
+					},
+				},
+			}},
+			exp: `{"serviceAccounts":{"TOTAL_COUNT":1,"namespaces":{"default":[{"name":"local-storage-operator","secrets":2}]}}}`,
 		},
 	}
 
@@ -51,33 +104,23 @@ func Test_ServiceAccounts_Gather(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
 			coreClient := kubefake.NewSimpleClientset()
-			for _, d := range test.data {
+			for _, namespace := range test.namespaces {
 				_, err := coreClient.CoreV1().Namespaces().Create(
 					context.Background(),
-					&corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: d.Namespace}}, metav1.CreateOptions{},
+					&corev1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: namespace}}, metav1.CreateOptions{},
 				)
-				if err != nil {
-					t.Fatalf("unable to create fake ns %s", err)
-				}
-				_, err = coreClient.CoreV1().ServiceAccounts(d.Namespace).
+				assert.NoError(t, err)
+			}
+			for _, d := range test.serviceAccounts {
+				_, err := coreClient.CoreV1().ServiceAccounts(d.Namespace).
 					Create(context.Background(), d, metav1.CreateOptions{})
-				if err != nil {
-					t.Fatalf("unable to create fake service account %s", err)
-				}
+				assert.NoError(t, err)
 			}
-			sa, errs := gatherServiceAccounts(context.Background(), coreClient.CoreV1())
-			if len(errs) > 0 {
-				t.Fatalf("unexpected errors: %#v", errs)
-				return
-			}
-			bts, err := sa[0].Item.Marshal()
-			if err != nil {
-				t.Fatalf("error marshaling %s", err)
-			}
-			s := string(bts)
-			if test.exp != s {
-				t.Fatalf("serviceaccount test failed. expected: %s got: %s", test.exp, s)
-			}
+			serviceAccounts, errs := gatherServiceAccounts(context.Background(), coreClient.CoreV1())
+			assert.Emptyf(t, errs, "Unexpected errors: %#v", errs)
+			bts, err := serviceAccounts[0].Item.Marshal()
+			assert.NoError(t, err)
+			assert.EqualValues(t, test.exp, string(bts))
 		})
 	}
 }
