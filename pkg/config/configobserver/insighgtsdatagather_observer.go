4355e1796cddd9a2ecdf167a21951f713790cdc6
Tomáš Remeš <tremes@redhat.com>
2023-06-08 14:07:26 +0200

    OCPBUGS-14771 run an extra config informer in the tech preview

--- pkg/config/configobserver/insighgtsdatagather_observer.go
+++ pkg/config/configobserver/insighgtsdatagather_observer.go
@@ -15,32 +15,30 @@ import (
 	"k8s.io/klog/v2"
 )
 
-type APIConfigObserver interface {
+type InsightsDataGatherObserver interface {
 	factory.Controller
 	GatherConfig() *v1alpha1.GatherConfig
 	GatherDataPolicy() *v1alpha1.DataPolicy
 	GatherDisabled() bool
 }
 
-type APIConfigController struct {
+type insightsDataGatherController struct {
 	factory.Controller
 	lock              sync.Mutex
-	listeners         map[chan *v1alpha1.GatherConfig]struct{}
 	configV1Alpha1Cli *configCliv1alpha1.ConfigV1alpha1Client
 	gatherConfig      *v1alpha1.GatherConfig
 }
 
-func NewAPIConfigObserver(kubeConfig *rest.Config,
+func NewInsightsDataGatherObserver(kubeConfig *rest.Config,
 	eventRecorder events.Recorder,
-	configInformer configinformers.SharedInformerFactory) (APIConfigObserver, error) {
+	configInformer configinformers.SharedInformerFactory) (InsightsDataGatherObserver, error) {
 	inf := configInformer.Config().V1alpha1().InsightsDataGathers().Informer()
 	configV1Alpha1Cli, err := configCliv1alpha1.NewForConfig(kubeConfig)
 	if err != nil {
 		return nil, err
 	}
-	c := &APIConfigController{
+	c := &insightsDataGatherController{
 		configV1Alpha1Cli: configV1Alpha1Cli,
-		listeners:         make(map[chan *v1alpha1.GatherConfig]struct{}),
 	}
 
 	insightDataGatherConf, err := c.configV1Alpha1Cli.InsightsDataGathers().Get(context.Background(), "cluster", metav1.GetOptions{})
@@ -51,42 +49,42 @@ func NewAPIConfigObserver(kubeConfig *rest.Config,
 
 	ctrl := factory.New().WithInformers(inf).
 		WithSync(c.sync).
-		ToController("InsightConfigController", eventRecorder)
+		ToController("InsightsDataGatherObserver", eventRecorder)
 	c.Controller = ctrl
 	return c, nil
 }
 
-func (a *APIConfigController) sync(ctx context.Context, _ factory.SyncContext) error {
-	insightDataGatherConf, err := a.configV1Alpha1Cli.InsightsDataGathers().Get(ctx, "cluster", metav1.GetOptions{})
+func (i *insightsDataGatherController) sync(ctx context.Context, scx factory.SyncContext) error {
+	insightDataGatherConf, err := i.configV1Alpha1Cli.InsightsDataGathers().Get(ctx, "cluster", metav1.GetOptions{})
 	if err != nil {
 		return err
 	}
-	a.gatherConfig = &insightDataGatherConf.Spec.GatherConfig
+	i.gatherConfig = &insightDataGatherConf.Spec.GatherConfig
 	return nil
 }
 
 // GatherConfig provides the complete gather config in a thread-safe way.
-func (a *APIConfigController) GatherConfig() *v1alpha1.GatherConfig {
-	a.lock.Lock()
-	defer a.lock.Unlock()
-	return a.gatherConfig
+func (i *insightsDataGatherController) GatherConfig() *v1alpha1.GatherConfig {
+	i.lock.Lock()
+	defer i.lock.Unlock()
+	return i.gatherConfig
 }
 
 // GatherDisabled tells whether data gathering is disabled or not
-func (a *APIConfigController) GatherDisabled() bool {
-	a.lock.Lock()
-	defer a.lock.Unlock()
+func (i *insightsDataGatherController) GatherDisabled() bool {
+	i.lock.Lock()
+	defer i.lock.Unlock()
 
-	if utils.StringInSlice("all", a.gatherConfig.DisabledGatherers) ||
-		utils.StringInSlice("ALL", a.gatherConfig.DisabledGatherers) {
+	if utils.StringInSlice("all", i.gatherConfig.DisabledGatherers) ||
+		utils.StringInSlice("ALL", i.gatherConfig.DisabledGatherers) {
 		return true
 	}
 	return false
 }
 
 // GatherDataPolicy provides DataPolicy attribute value defined in the API
-func (a *APIConfigController) GatherDataPolicy() *v1alpha1.DataPolicy {
-	a.lock.Lock()
-	defer a.lock.Unlock()
-	return &a.gatherConfig.DataPolicy
+func (i *insightsDataGatherController) GatherDataPolicy() *v1alpha1.DataPolicy {
+	i.lock.Lock()
+	defer i.lock.Unlock()
+	return &i.gatherConfig.DataPolicy
 }
