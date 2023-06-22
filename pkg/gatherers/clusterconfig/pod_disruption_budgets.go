e63fc528bfab10a83311a2cf933422c3bb578220
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2023-06-22 09:57:23 +0000

    [release-4.12] OCPBUGS-14318: gather PDBs only from openshift namespaces (#786)
    * Gather PDB only from openshift namespace
    
    * minor docs update
    
    * minor unit test refactor
    
    * fix based on review

--- pkg/gatherers/clusterconfig/pod_disruption_budgets.go
+++ pkg/gatherers/clusterconfig/pod_disruption_budgets.go
@@ -3,6 +3,7 @@ package clusterconfig
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	policyclient "k8s.io/client-go/kubernetes/typed/policy/v1"
@@ -11,7 +12,7 @@ import (
 )
 
 const (
-	gatherPodDisruptionBudgetLimit = 5000
+	gatherPodDisruptionBudgetLimit = 100
 )
 
 // GatherPodDisruptionBudgets gathers the cluster's PodDisruptionBudgets.
@@ -19,13 +20,25 @@ const (
 // The Kubernetes api https://github.com/kubernetes/client-go/blob/v11.0.0/kubernetes/typed/policy/v1beta1/poddisruptionbudget.go#L80
 // Response see https://docs.okd.io/latest/rest_api/policy_apis/poddisruptionbudget-policy-v1beta1.html
 //
-// * Location in archive: config/pdbs/
-// * See: docs/insights-archive-sample/config/pdbs
-// * Id in config: clusterconfig/pdbs
-// * Since versions:
-//   - 4.4.30+
-//   - 4.5.34+
-//   - 4.6+
+// ### Sample data
+// - docs/insights-archive-sample/config/pdbs/openshift-machine-config-operator/etcd-quorum-guard.json
+//
+// ### Location in archive
+// - `config/pdbs/{namespace}/{name}.json`
+//
+// ### Config ID
+// `clusterconfig/pdbs`
+//
+// ### Released version
+// - 4.6.0
+//
+// ### Backported versions
+// - 4.4.30+
+// - 4.5.15+
+//
+// ### Changes
+// - The gatherer was changed to gather pdbs only from namespaces with "openshift" prefix
+// and the limit of gathered records to 100 since 4.14.
 func (g *Gatherer) GatherPodDisruptionBudgets(ctx context.Context) ([]record.Record, []error) {
 	gatherPolicyClient, err := policyclient.NewForConfig(g.gatherKubeConfig)
 	if err != nil {
@@ -36,20 +49,23 @@ func (g *Gatherer) GatherPodDisruptionBudgets(ctx context.Context) ([]record.Rec
 }
 
 func gatherPodDisruptionBudgets(ctx context.Context, policyClient policyclient.PolicyV1Interface) ([]record.Record, []error) {
-	pdbs, err := policyClient.PodDisruptionBudgets("").List(ctx, metav1.ListOptions{Limit: gatherPodDisruptionBudgetLimit})
+	pdbs, err := policyClient.PodDisruptionBudgets("").List(ctx, metav1.ListOptions{})
 	if err != nil {
 		return nil, []error{err}
 	}
 	var records []record.Record
 	for i := range pdbs.Items {
-		recordName := fmt.Sprintf("config/pdbs/%s", pdbs.Items[i].GetName())
-		if pdbs.Items[i].GetNamespace() != "" {
-			recordName = fmt.Sprintf("config/pdbs/%s/%s", pdbs.Items[i].GetNamespace(), pdbs.Items[i].GetName())
+		item := &pdbs.Items[i]
+		if strings.HasPrefix(item.GetNamespace(), "openshift-") {
+			recordName := fmt.Sprintf("config/pdbs/%s/%s", item.GetNamespace(), item.GetName())
+			records = append(records, record.Record{
+				Name: recordName,
+				Item: record.ResourceMarshaller{Resource: item},
+			})
+			if len(records) == gatherPodDisruptionBudgetLimit {
+				break
+			}
 		}
-		records = append(records, record.Record{
-			Name: recordName,
-			Item: record.ResourceMarshaller{Resource: &pdbs.Items[i]},
-		})
 	}
 	return records, nil
 }
