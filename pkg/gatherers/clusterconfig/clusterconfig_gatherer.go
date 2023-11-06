2768368cfaa00b59e484d3ab5ff30c04770a5503
Ricardo Lüders <rluders@redhat.com>
2023-11-06 11:08:52 +0100

    OCPBUGS-22958: adds cluster storageclasses gather (#858)
    * feat(gather): adds cluster storageclasses
    
    docs: updated gathered data
    
    * refactor: renaming gather after review

--- pkg/gatherers/clusterconfig/clusterconfig_gatherer.go
+++ pkg/gatherers/clusterconfig/clusterconfig_gatherer.go
@@ -83,6 +83,7 @@ var gatheringFunctions = map[string]gathererFuncPtr{
 	"scheduler_logs":                    (*Gatherer).GatherSchedulerLogs,
 	"service_accounts":                  (*Gatherer).GatherServiceAccounts,
 	"silenced_alerts":                   (*Gatherer).GatherSilencedAlerts,
+	"storage_classes":                   (*Gatherer).GatherStorageClasses,
 	"storage_cluster":                   (*Gatherer).GatherStorageCluster,
 	"support_secret":                    (*Gatherer).GatherSupportSecret,
 	"tsdb_status":                       (*Gatherer).GatherPrometheusTSDBStatus,
