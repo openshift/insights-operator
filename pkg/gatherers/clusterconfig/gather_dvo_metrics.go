67c4a7288855e28a81e7173cccbccdb9c3b88586
Tomas Remes <tremes@users.noreply.github.com>
2023-11-09 14:40:25 +0100

    DVO gatherer - do the retry request as HEAD and not stream (#861)

--- pkg/gatherers/clusterconfig/gather_dvo_metrics.go
+++ pkg/gatherers/clusterconfig/gather_dvo_metrics.go
@@ -137,15 +137,21 @@ func gatherDVOMetricsFromEndpoint(
 		http.DefaultClient,
 	)
 	if err != nil {
-		klog.Warningf("Unable to load metrics client, no metrics will be collected: %v", err)
+		klog.Errorf("Unable to load metrics client, no metrics will be collected: %v", err)
 		return nil, err
 	}
 
-	dataReader, err := queryMetricsService(ctx, metricsRESTClient)
+	err = metricsServiceUp(ctx, metricsRESTClient, apiURL)
 	if err != nil {
-		klog.Warningf("Unable to retrieve DVO metrics: %v", err)
 		return nil, err
 	}
+
+	dataReader, err := metricsRESTClient.Get().AbsPath("metrics").Stream(ctx)
+	if err != nil {
+		klog.Errorf("Failed to stream data from the DVO service: %v", err)
+		return nil, err
+	}
+
 	defer func() {
 		// The error variable must have a more unique name to satisfy govet.
 		if closeErr := dataReader.Close(); closeErr != nil {
@@ -181,22 +187,24 @@ func gatherDVOMetricsFromEndpoint(
 	return prefixedLines, nil
 }
 
-// queryMetricsService queries the DVO metrics service with poll in case of an error. Polling
-// timeout is 2 seconds and interval is 0.5 seconds.
-func queryMetricsService(ctx context.Context, client *rest.RESTClient) (io.ReadCloser, error) {
-	var dataReader io.ReadCloser
-	err := wait.PollUntilContextTimeout(ctx, 1*time.Second, 5*time.Second, true, func(ctx context.Context) (done bool, err error) {
-		dataReader, err = client.Get().AbsPath("metrics").Stream(ctx)
-		if err != nil {
+// metricsServiceUp queries the DVO metrics service with poll in case of an error. Polling
+// timeout is 5 seconds and interval is 1 second.
+func metricsServiceUp(ctx context.Context, client *rest.RESTClient, apiURL *url.URL) error {
+	timeout := 5 * time.Second
+	dvoMetricsURL := fmt.Sprintf("http://%s/metrics", apiURL.Host)
+	err := wait.PollUntilContextTimeout(ctx, 1*time.Second, timeout, true, func(ctx context.Context) (done bool, err error) {
+		resp, err := client.Client.Head(dvoMetricsURL) //nolint: noctx
+		if err != nil || resp.StatusCode != 200 {
 			klog.Warning("Failed to read the DVO metrics. Trying again.")
 			return false, nil
 		}
+		defer resp.Body.Close()
 		return true, nil
 	})
 	if err != nil {
-		return nil, fmt.Errorf("failed to read the data from the DVO metrics service: %v", err)
+		return fmt.Errorf("DVO metrics service was not available within the %s timeout: %v", timeout, err)
 	}
-	return dataReader, nil
+	return nil
 }
 
 // obfuscateDVOMetrics tells whether DVO metrics should be "obfuscated" or not
