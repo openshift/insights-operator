1826706c6e714e5ebbb7af93d6bbd20da024d845
Ricardo Lüders <rluders@redhat.com>
2024-03-06 09:28:49 +0100

    feat: ingress certificates gather

--- pkg/gatherers/clusterconfig/clusterconfig_gatherer.go
+++ pkg/gatherers/clusterconfig/clusterconfig_gatherer.go
@@ -45,6 +45,7 @@ var gatheringFunctions = map[string]gathererFuncPtr{
 	"image_registries":                  (*Gatherer).GatherClusterImageRegistry,
 	"infrastructures":                   (*Gatherer).GatherClusterInfrastructure,
 	"ingress":                           (*Gatherer).GatherClusterIngress,
+	"ingress_certificates":              (*Gatherer).GatherClusterIngressCertificates,
 	"install_plans":                     (*Gatherer).GatherInstallPlans,
 	"jaegers":                           (*Gatherer).GatherJaegerCR,
 	"kube_controller_manager_logs":      (*Gatherer).GatherKubeControllerManagerLogs,
