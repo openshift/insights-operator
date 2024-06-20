de96b020c7d189427144aa3fccbd190767115ce3
Jeff Mesnil <jmesnil@redhat.com>
2024-06-20 14:01:45 +0200

    Update insights-operator-gather cluster role so that it can pod/exec on the insights-operator-runtime
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

--- deploy-insights-operator-with-runtime.sh
+++ deploy-insights-operator-with-runtime.sh
@@ -4,6 +4,7 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO111MODULE=on go build -a -o ./bin/insigh
 podman build --platform linux/amd64 -t quay.io/jmesnil/insights-operator-with-runtime -f ./Dockerfile-insights-operator-runtime .
 podman push quay.io/jmesnil/insights-operator-with-runtime
 
-oc apply -f ./manifests/06a-deployment-with-runtime.yaml -n openshift-insights
-oc apply -f ./manifests/010-clusterrole-insights-operator-runtime.yaml -n openshift-insights
-oc apply -f ./manifests/010-insights-operator-runtime.yaml -n openshift-insights
+oc apply -f .//manifests/03-clusterrole.yaml
+oc apply -f ./manifests/06a-deployment-with-runtime.yaml
+oc apply -f ./manifests/010-clusterrole-insights-operator-runtime.yaml
+oc apply -f ./manifests/010-insights-operator-runtime.yaml
