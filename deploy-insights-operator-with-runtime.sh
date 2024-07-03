29a03ecf4f65fe00c35020735253fcf894cca4c5
Jeff Mesnil <jmesnil@redhat.com>
2024-07-03 11:15:22 +0200

    Rename insights-operator-runtime to insights-runtime-extractor
    The image name still needs to be updated
    
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

--- deploy-insights-operator-with-runtime.sh
+++ deploy-insights-operator-with-runtime.sh
@@ -1,10 +1,9 @@
 # Use go build instead of make build to target linux/amd64
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO111MODULE=on go build -a -o ./bin/insights-operator -ldflags="${GO_LDFLAGS}" ./cmd/insights-operator/main.go
 
-podman build --platform linux/amd64 -t quay.io/jmesnil/insights-operator-with-runtime -f ./Dockerfile-insights-operator-runtime .
+podman build --platform linux/amd64 -t quay.io/jmesnil/insights-operator-with-runtime -f Dockerfile-insights-operator-with-runtime-extractor .
 podman push quay.io/jmesnil/insights-operator-with-runtime
 
-oc apply -f .//manifests/03-clusterrole.yaml
 oc apply -f ./manifests/06a-deployment-with-runtime.yaml
-oc apply -f ./manifests/010-clusterrole-insights-operator-runtime.yaml
-oc apply -f ./manifests/010-insights-operator-runtime.yaml
+oc apply -f ./manifests/010-clusterrole-insights-runtime-extractor.yaml
+oc apply -f ./manifests/010-insights-runtime-extractor.yaml
