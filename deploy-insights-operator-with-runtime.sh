c4ede8bb06bda97a048a5b06c31adf1e1ce8b413
Jeff Mesnil <jmesnil@redhat.com>
2024-09-05 13:11:45 +0200

    Update security constraints
    so that the namespace allows to run container with privileged permissions
    
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

--- deploy-insights-operator-with-runtime.sh
+++ deploy-insights-operator-with-runtime.sh
@@ -4,6 +4,7 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 GO111MODULE=on go build -a -o ./bin/insigh
 podman build --platform linux/amd64 -t quay.io/jmesnil/insights-operator-with-runtime -f Dockerfile-insights-operator-with-runtime-extractor .
 podman push quay.io/jmesnil/insights-operator-with-runtime
 
+oc apply -f ./manifests/02-namespace.yaml
 oc apply -f ./manifests/06a-deployment-with-runtime.yaml
 oc apply -f ./manifests/010-clusterrole-insights-runtime-extractor.yaml
 oc apply -f ./manifests/010-insights-runtime-extractor-service.yaml
