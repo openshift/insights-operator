24886decac7371a872ad1a09269f883af5d6fdc8
José Luis Segura Lucas <joseluis@redhat.com>
2024-11-13 16:39:15 +0100

    Gathering at most 20. Anonymizing the results to remove tenant's selectors

--- pkg/gatherers/clusterconfig/gather_lokistack.go
+++ pkg/gatherers/clusterconfig/gather_lokistack.go
@@ -5,7 +5,6 @@ package clusterconfig
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/klog/v2"
@@ -15,9 +14,9 @@ import (
 	"k8s.io/client-go/dynamic"
 
 	"github.com/openshift/insights-operator/pkg/record"
-	"github.com/openshift/insights-operator/pkg/utils"
-	"github.com/openshift/insights-operator/pkg/utils/anonymize"
 )
+
+const LokiStackResourceLimit = 20
 
 // GatherOpenshiftLogging Collects `clusterlogging.logging.openshift.io` resources.
 //
@@ -28,13 +27,13 @@ import (
 // - docs/insights-archive-sample/config/logging/openshift-logging/instance.json
 //
 // ### Location in archive
-// - `config/logging/{namespace}/{name}.json`
+// - `namespace/openshift-logging/group/resource/{name}.json`
 //
 // ### Config ID
 // `clusterconfig/openshift_logging`
 //
 // ### Released version
-// - 4.9.0
+// - 4.18.0
 //
 // ### Backported versions
 // None
@@ -51,7 +50,8 @@ func (g *Gatherer) GatherLokiStack(ctx context.Context) ([]record.Record, []erro
 }
 
 func gatherLokiStack(ctx context.Context, dynamicClient dynamic.Interface) ([]record.Record, []error) {
-	loggingResourceList, err := dynamicClient.Resource(lokiStackResource).List(ctx, metav1.ListOptions{})
+	klog.V(2).Info("Start LokiStack gathering")
+	loggingResourceList, err := dynamicClient.Resource(lokiStackResource).List(ctx, metav1.ListOptions{Limit: LokiStackResourceLimit})
 	if errors.IsNotFound(err) {
 		return nil, nil
 	}
@@ -60,40 +60,72 @@ func gatherLokiStack(ctx context.Context, dynamicClient dynamic.Interface) ([]re
 		return nil, []error{err}
 	}
 
-	var records []record.Record
-	for i := range loggingResourceList.Items {
-		item := loggingResourceList.Items[i]
-		err = anonymizeLokiStackNamespace(item.Object)
-		if err != nil {
-			// not storing resource with non-anonymized data
-			continue
+	var errs []error
+
+	numItems := len(loggingResourceList.Items)
+	if numItems == 1 {
+		klog.V(2).Info("There is only one LokiStack. Let's get the Namespace")
+		item := loggingResourceList.Items[0]
+		if item.GetNamespace() != "openshift-logging" {
+			return nil, []error{fmt.Errorf("Found resource, but in wrong namespace")}
 		}
-		records = append(records, record.Record{
-			Name: fmt.Sprintf("config/lokistack/%s/%s", item.GetUID(), item.GetName()),
-			Item: record.ResourceMarshaller{Resource: &item},
-		})
+
+		if err := removeLimitsTenant(item.Object); err != nil {
+			return []record.Record{}, []error{err}
+		}
+
+		return []record.Record{
+			{
+				Name: fmt.Sprintf(
+					"namespace/%s/%s/%s/%s",
+					item.GetNamespace(),
+					lokiStackResource.Group,
+					lokiStackResource.Resource,
+					item.GetName()),
+				Item: record.ResourceMarshaller{Resource: &item},
+			},
+		}, nil
+
+	} else if numItems > 1 {
+		errs = append(errs, fmt.Errorf("Found more resources than expected (expected 1)"))
 	}
-	return records, nil
+
+	if loggingResourceList.GetContinue() != "" {
+		errs = append(errs, fmt.Errorf("Found more than %d resources", LokiStackResourceLimit))
+	}
+
+	// numItems == 0
+	return nil, errs
 }
 
-// anonymizeLokiStackNamespace tries to get an array of sensitive fields defined in the LokiStack
+// removeLimitsTenant tries to get an array of sensitive fields defined in the LokiStack
 // and anonymize potentially sensitive data - e.g. url, credentials
-func anonymizeLokiStackNamespace(obj map[string]interface{}) error {
-	namespace, err := utils.NestedStringWrapper(obj, "metadata", "namespace")
-	if err != nil {
-		// namespace not found, weird, but ok. Return silently
-		klog.V(2).Info("Resource doesn't have namespace")
-		return nil
+func removeLimitsTenant(obj map[string]interface{}) error {
+	// unstructured.RemoveNestedField(obj, "spec", "limits", "tenants", "application", "streams", "selector")
+	// unstructured.RemoveNestedField(obj, "spec", "limits", "tenants", "audit", "streams", "selector")
+	// unstructured.RemoveNestedField(obj, "spec", "limits", "tenants", "infrastructure", "streams", "selector")
+
+	for _, tenant := range []string{"application", "infrastructure", "audit"} {
+		klog.V(2).Infof("Anonymizing %s tenant", tenant)
+		streamSlice, ok, err := unstructured.NestedSlice(obj, "spec", "limits", "tenants", tenant, "retention", "streams")
+		if err != nil {
+			klog.V(2).Infof("Bad structure for the gathered file: %v", err)
+			return err
+		} else if !ok {
+			// tenant not found
+			continue
+		}
+
+		for _, stream := range streamSlice {
+			streamMap, ok := stream.(map[string]interface{})
+			if !ok {
+				continue
+			}
+			unstructured.RemoveNestedField(streamMap, "selector")
+		}
+
+		unstructured.SetNestedSlice(obj, streamSlice, "spec", "limits", "tenants", tenant, "retention", "streams")
 	}
 
-	if strings.HasPrefix(namespace, "openshift-") {
-		// These namespaces don't need anonymization
-		return nil
-	}
-
-	err = unstructured.SetNestedField(obj, anonymize.String(namespace), "metadata", "namespace")
-	if err != nil {
-		klog.V(2).ErrorS(err, "Failed to anonymize the namespace")
-	}
-	return err
+	return nil
 }
