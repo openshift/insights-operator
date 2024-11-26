d3cbf1f383c2d4b4d419084a4bd62e8b16d7f6e6
José Luis Segura Lucas <joseluis@redhat.com>
2024-11-26 23:32:18 +0100

    Tests table-strategy

--- pkg/gatherers/clusterconfig/gather_lokistacks_test.go
+++ pkg/gatherers/clusterconfig/gather_lokistacks_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -13,141 +14,7 @@ import (
 	dynamicfake "k8s.io/client-go/dynamic/fake"
 )
 
-func Test_LokiStacks_Gather(t *testing.T) {
-	var lokiStackYAML = `
-apiVersion: loki.grafana.com/v1
-kind: LokiStack
-metadata:
-    name: test-lokistack
-    namespace: openshift-logging
-`
-
-	client := dynamicfake.NewSimpleDynamicClientWithCustomListKinds(runtime.NewScheme(), map[schema.GroupVersionResource]string{
-		lokiStackResource: "LokiStacksList",
-	})
-	decUnstructured := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
-
-	testLokiStack := &unstructured.Unstructured{}
-
-	_, _, err := decUnstructured.Decode([]byte(lokiStackYAML), nil, testLokiStack)
-	if err != nil {
-		t.Fatal("unable to decode lokistack ", err)
-	}
-	_, err = client.Resource(lokiStackResource).
-		Namespace("openshift-logging").
-		Create(context.Background(), testLokiStack, metav1.CreateOptions{})
-	if err != nil {
-		t.Fatal("unable to create fake lokistack ", err)
-	}
-
-	ctx := context.Background()
-	records, errs := gatherLokiStack(ctx, client)
-	if len(errs) > 0 {
-		t.Errorf("unexpected errors: %#v", errs)
-		return
-	}
-	if len(records) != 1 {
-		t.Fatalf("unexpected number or records %d", len(records))
-	}
-	if records[0].Name != "namespace/openshift-logging/loki.grafana.com/lokistacks/test-lokistack" {
-		t.Fatalf("unexpected lokistack name %s", records[0].Name)
-	}
-}
-
-func Test_LokiStacks_Gather_SeveralResourcesRightNamespace(t *testing.T) {
-	var lokiStackYAMLTmpl = `
-apiVersion: loki.grafana.com/v1
-kind: LokiStack
-metadata:
-    name: test-lokistack-%d
-    namespace: openshift-logging
-`
-
-	client := dynamicfake.NewSimpleDynamicClientWithCustomListKinds(runtime.NewScheme(), map[schema.GroupVersionResource]string{
-		lokiStackResource: "LokiStacksList",
-	})
-	decUnstructured := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
-
-	testLokiStack := &unstructured.Unstructured{}
-
-	for idx := 0; idx < lokiStackResourceLimit; idx++ {
-		lokiStackYAML := fmt.Sprintf(lokiStackYAMLTmpl, idx)
-		_, _, err := decUnstructured.Decode([]byte(lokiStackYAML), nil, testLokiStack)
-		if err != nil {
-			t.Fatal("unable to decode lokistack ", err)
-		}
-		_, err = client.Resource(lokiStackResource).
-			Namespace("openshift-logging").
-			Create(context.Background(), testLokiStack, metav1.CreateOptions{})
-		if err != nil {
-			t.Fatal("unable to create fake lokistack ", err)
-		}
-	}
-
-	ctx := context.Background()
-	records, errs := gatherLokiStack(ctx, client)
-	if len(errs) > 0 {
-		t.Errorf("unexpected errors: %#v", errs)
-		return
-	}
-	if len(records) != lokiStackResourceLimit {
-		t.Fatalf("unexpected number or records %d", len(records))
-	}
-}
-
-func Test_LokiStacks_Gather_TooManyResources(t *testing.T) {
-	var lokiStackYAMLTmpl = `
-apiVersion: loki.grafana.com/v1
-kind: LokiStack
-metadata:
-    name: test-lokistack-%d
-    namespace: openshift-logging
-`
-
-	client := dynamicfake.NewSimpleDynamicClientWithCustomListKinds(runtime.NewScheme(), map[schema.GroupVersionResource]string{
-		lokiStackResource: "LokiStacksList",
-	})
-	decUnstructured := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
-
-	testLokiStack := &unstructured.Unstructured{}
-
-	// prepare limit + 1 resources in the right namespace
-	for idx := 0; idx < lokiStackResourceLimit+1; idx++ {
-		lokiStackYAML := fmt.Sprintf(lokiStackYAMLTmpl, idx)
-		_, _, err := decUnstructured.Decode([]byte(lokiStackYAML), nil, testLokiStack)
-		if err != nil {
-			t.Fatal("unable to decode lokistack ", err)
-		}
-		_, err = client.Resource(lokiStackResource).
-			Namespace("openshift-logging").
-			Create(context.Background(), testLokiStack, metav1.CreateOptions{})
-		if err != nil {
-			t.Fatal("unable to create fake lokistack ", err)
-		}
-	}
-
-	ctx := context.Background()
-	records, errs := gatherLokiStack(ctx, client)
-
-	// ensuring an error was received
-	if len(errs) != 1 {
-		t.Errorf("1 error was expected, %d received", len(errs))
-		return
-	}
-
-	if errs[0].Error() != fmt.Sprintf(
-		"found %d resources, limit (%d) reached", lokiStackResourceLimit+1, lokiStackResourceLimit,
-	) {
-		t.Fatalf("unexpected error recorded: %#v", errs[0])
-	}
-
-	// only expect "limit" number of records
-	if len(records) != lokiStackResourceLimit {
-		t.Fatalf("unexpected number or records %d", len(records))
-	}
-}
-
-func Test_LokiStacks_Gather_OtherNamespaces(t *testing.T) {
+func TestGatherLokiStacks(t *testing.T) {
 	var lokiStackYAMLTmpl = `
 apiVersion: loki.grafana.com/v1
 kind: LokiStack
@@ -156,42 +23,73 @@ metadata:
     namespace: %s
 `
 
-	client := dynamicfake.NewSimpleDynamicClientWithCustomListKinds(runtime.NewScheme(), map[schema.GroupVersionResource]string{
-		lokiStackResource: "LokiStacksList",
-	})
-	decUnstructured := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+	tests := []struct {
+		name                    string
+		namespace               string
+		resourcesNumber         int
+		expectedErrors          []error
+		expectedNumberOfRecords int
+	}{
+		{
+			name:                    "one resource scenario",
+			namespace:               "openshift-logging",
+			resourcesNumber:         1,
+			expectedErrors:          nil,
+			expectedNumberOfRecords: 1,
+		},
+		{
+			name:                    "several resources in right namespace",
+			namespace:               "openshift-logging",
+			resourcesNumber:         lokiStackResourceLimit,
+			expectedErrors:          nil,
+			expectedNumberOfRecords: lokiStackResourceLimit,
+		},
+		{
+			name:            "too many resources in right namespace",
+			namespace:       "openshift-logging",
+			resourcesNumber: lokiStackResourceLimit + 1,
+			expectedErrors: []error{
+				fmt.Errorf("found %d resources, limit (%d) reached", lokiStackResourceLimit+1, lokiStackResourceLimit),
+			},
+			expectedNumberOfRecords: lokiStackResourceLimit,
+		},
+		{
+			name:            "bad namespace",
+			namespace:       "other-namespace",
+			resourcesNumber: 1,
+			expectedErrors: []error{
+				fmt.Errorf("found resource in an unexpected namespace"),
+			},
+			expectedNumberOfRecords: 0,
+		},
+	}
 
-	testLokiStack := &unstructured.Unstructured{}
+	for _, tt := range tests {
+		client := dynamicfake.NewSimpleDynamicClientWithCustomListKinds(runtime.NewScheme(), map[schema.GroupVersionResource]string{
+			lokiStackResource: "LokiStacksList",
+		})
+		decUnstructured := yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+		testLokiStack := &unstructured.Unstructured{}
 
-	for idx, namespace := range []string{"openshift-logging", "other-namespace"} {
-		lokiStackYAML := fmt.Sprintf(lokiStackYAMLTmpl, idx, namespace)
-		_, _, err := decUnstructured.Decode([]byte(lokiStackYAML), nil, testLokiStack)
-		if err != nil {
-			t.Fatal("unable to decode lokistack ", err)
+		for idx := 0; idx < tt.resourcesNumber; idx++ {
+			lokiStackYAML := fmt.Sprintf(lokiStackYAMLTmpl, idx, tt.namespace)
+			_, _, err := decUnstructured.Decode([]byte(lokiStackYAML), nil, testLokiStack)
+			if err != nil {
+				t.Fatal("unable to decode lokistack ", err)
+			}
+			_, err = client.Resource(lokiStackResource).
+				Namespace(tt.namespace).
+				Create(context.Background(), testLokiStack, metav1.CreateOptions{})
+			if err != nil {
+				t.Fatal("unable to create fake lokistack ", err)
+			}
 		}
-		_, err = client.Resource(lokiStackResource).
-			Namespace(namespace).
-			Create(context.Background(), testLokiStack, metav1.CreateOptions{})
-		if err != nil {
-			t.Fatal("unable to create fake lokistack ", err)
-		}
+
+		ctx := context.Background()
+		records, errs := gatherLokiStack(ctx, client)
+
+		assert.Equal(t, tt.expectedNumberOfRecords, len(records))
+		assert.Equal(t, tt.expectedErrors, errs)
 	}
 
-	ctx := context.Background()
-	records, errs := gatherLokiStack(ctx, client)
-
-	// ensuring an error was received
-	if len(errs) != 1 {
-		t.Errorf("1 error was expected, %d received", len(errs))
-		return
-	}
-
-	if errs[0].Error() != "found resource in an unexpected namespace" {
-		t.Fatalf("unexpected error recorded: %#v", errs[0])
-	}
-
-	// only expect "limit" number of records
-	if len(records) != 1 {
-		t.Fatalf("unexpected number or records %d", len(records))
-	}
 }
