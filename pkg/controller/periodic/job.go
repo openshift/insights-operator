88b87964c105cf45d494327188757a809cf3849a
Ondrej Pokorny <opokorny@redhat.com>
2025-01-28 13:04:54 +0100

    feat: use persistent storage for on-demand gathering
    This commit adds new field to the config map that
    allows users to specify PVC that should be used to
    store archives created by on-demand gathering.
    
    Signed-off-by: Ondrej Pokorny <opokorny@redhat.com>

--- pkg/controller/periodic/job.go
+++ pkg/controller/periodic/job.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/openshift/insights-operator/pkg/config"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -13,6 +14,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/watch"
+	"k8s.io/klog/v2"
 )
 
 // JobController type responsible for
@@ -29,7 +31,11 @@ func NewJobController(kubeClient kubernetes.Interface) *JobController {
 
 // CreateGathererJob creates a new Kubernetes Job with provided image, volume mount path used for storing data archives and name
 // derived from the provided data gather name
-func (j *JobController) CreateGathererJob(ctx context.Context, dataGatherName, image, archiveVolumeMountPath string) (*batchv1.Job, error) {
+func (j *JobController) CreateGathererJob(ctx context.Context, dataGatherName, image string, dataReporting config.DataReporting) (*batchv1.Job, error) {
+	// TODO: also prunning of the old jobs data needs to be implemented
+
+	volumeSource := j.createVolumeSource(ctx, dataReporting.PersistentVolumeClaimName)
+
 	gj := &batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      dataGatherName,
@@ -54,10 +60,8 @@ func (j *JobController) CreateGathererJob(ctx context.Context, dataGatherName, i
 					},
 					Volumes: []corev1.Volume{
 						{
-							Name: "archives-path",
-							VolumeSource: corev1.VolumeSource{
-								EmptyDir: &corev1.EmptyDirVolumeSource{},
-							},
+							Name:         "archives-path",
+							VolumeSource: volumeSource,
 						},
 						{
 							Name: serviceCABundle,
@@ -99,7 +103,7 @@ func (j *JobController) CreateGathererJob(ctx context.Context, dataGatherName, i
 							VolumeMounts: []corev1.VolumeMount{
 								{
 									Name:      "archives-path",
-									MountPath: archiveVolumeMountPath,
+									MountPath: dataReporting.StoragePath,
 								},
 								{
 									Name:      serviceCABundle,
@@ -158,5 +162,23 @@ func (j *JobController) WaitForJobCompletion(ctx context.Context, job *batchv1.J
 				return fmt.Errorf("job %s failed", job.Name)
 			}
 		}
+	}
+}
+
+func (j *JobController) createVolumeSource(ctx context.Context, persistentVolumeClaimName string) corev1.VolumeSource {
+	pvc, err := j.kubeClient.CoreV1().PersistentVolumeClaims(insightsNamespace).Get(ctx, persistentVolumeClaimName, metav1.GetOptions{})
+	if err == nil && pvc != nil {
+		klog.Infof("Creating volume source with PersistentVolumeClaimName: %s", persistentVolumeClaimName)
+		return corev1.VolumeSource{
+			PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+				ClaimName: persistentVolumeClaimName,
+			},
+		}
+	}
+
+	klog.Infof("Unable to get PersistentVolumeClaim: %v", err)
+	klog.Info("Creating volume source with EmptyDir")
+	return corev1.VolumeSource{
+		EmptyDir: &corev1.EmptyDirVolumeSource{},
 	}
 }
