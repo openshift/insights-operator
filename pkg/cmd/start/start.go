de2dee5a73eebcbe6cec7e340d00d81471029ea3
Ondrej Pokorny <98944165+opokornyy@users.noreply.github.com>
2025-03-20 12:27:31 +0100

    NO-JIRA: refactor gather commands (#1076)
    * feat: refactor runGather and runGatherAndUpload
    
    This commit is removing the code duplication in a
    runGather and runaGatherAndUpload functions.
    
    Signed-off-by: Ondrej Pokorny <opokorny@redhat.com>
    
    * feat: remove code duplication
    
    Signed-off-by: Ondrej Pokorny <opokorny@redhat.com>
    
    ---------
    
    Signed-off-by: Ondrej Pokorny <opokorny@redhat.com>

--- pkg/cmd/start/start.go
+++ pkg/cmd/start/start.go
@@ -105,94 +105,8 @@ func NewGatherAndUpload() *cobra.Command {
 		Run:   runGatherAndUpload(operator, cfg),
 	}
 	cmd.Flags().AddFlagSet(cfg.NewCommand().Flags())
+
 	return cmd
-}
-
-// Starts a single gather, main responsibility is loading in the necessary configs.
-func runGather(operator *controller.GatherJob, cfg *controllercmd.ControllerCommandConfig) func(cmd *cobra.Command, _ []string) {
-	return func(cmd *cobra.Command, _ []string) {
-		if configArg := cmd.Flags().Lookup("config").Value.String(); len(configArg) == 0 {
-			klog.Exit("error: --config is required")
-		}
-		unstructured, _, _, err := cfg.Config()
-		if err != nil {
-			klog.Exit(err)
-		}
-		cont, err := config.LoadConfig(operator.Controller, unstructured.Object, config.ToDisconnectedController)
-		if err != nil {
-			klog.Exit(err)
-		}
-		operator.Controller = cont
-
-		var clientConfig *rest.Config
-		if kubeConfigPath := cmd.Flags().Lookup("kubeconfig").Value.String(); len(kubeConfigPath) > 0 {
-			kubeConfigBytes, err := os.ReadFile(kubeConfigPath) //nolint: govet
-			if err != nil {
-				klog.Exit(err)
-			}
-			kubeConfig, err := clientcmd.NewClientConfigFromBytes(kubeConfigBytes)
-			if err != nil {
-				klog.Exit(err)
-			}
-			clientConfig, err = kubeConfig.ClientConfig()
-			if err != nil {
-				klog.Exit(err)
-			}
-		} else {
-			clientConfig, err = rest.InClusterConfig()
-			if err != nil {
-				klog.Exit(err)
-			}
-		}
-		protoConfig := rest.CopyConfig(clientConfig)
-		protoConfig.AcceptContentTypes = pbAcceptContentTypes
-		protoConfig.ContentType = pbContentType
-
-		ctx, cancel := context.WithTimeout(context.Background(), operator.Interval)
-
-		// before calling the Gather logic, this command must check the featuregate status.
-		configClient, err := configv1client.NewForConfig(clientConfig)
-		if err != nil {
-			klog.Exitf("error: building client: %v", err)
-		}
-		configInformers := configv1informers.NewSharedInformerFactory(configClient, 10*time.Minute)
-
-		desiredVersion := missingVersion
-		if envVersion, exists := os.LookupEnv("RELEASE_VERSION"); exists {
-			desiredVersion = envVersion
-		}
-
-		// By default, this will exit(0) the process if the featuregates ever change to a different set of values.
-		featureGateAccessor := featuregates.NewFeatureGateAccess(
-			desiredVersion, missingVersion,
-			configInformers.Config().V1().ClusterVersions(), configInformers.Config().V1().FeatureGates(),
-			events.NewLoggingEventRecorder("insights-gather", clock.RealClock{}),
-		)
-		go featureGateAccessor.Run(ctx)
-		go configInformers.Start(ctx.Done())
-
-		select {
-		case <-featureGateAccessor.InitialFeatureGatesObserved():
-			featureGates, _ := featureGateAccessor.CurrentFeatureGates()
-			klog.Infof("FeatureGates initialized: knownFeatureGates=%v", featureGates.KnownFeatures())
-		case <-time.After(1 * time.Minute):
-			klog.Errorf("timed out waiting for FeatureGate detection")
-			klog.Exit(fmt.Errorf("timed out waiting for FeatureGate detection"))
-		}
-
-		featureGates, err := featureGateAccessor.CurrentFeatureGates()
-		if err != nil {
-			klog.Exit(err)
-		}
-		operator.InsightsConfigAPIEnabled = featureGates.Enabled(features.FeatureGateInsightsConfigAPI)
-		operator.RuntimeExtractorEnabled = featureGates.Enabled(features.FeatureGateInsightsRuntimeExtractor)
-		err = operator.Gather(ctx, clientConfig, protoConfig)
-		if err != nil {
-			klog.Error(err)
-		}
-		cancel()
-		os.Exit(0)
-	}
 }
 
 // Boilerplate for running an operator and handling command line arguments.
@@ -248,90 +162,127 @@ func runOperator(operator *controller.Operator, cfg *controllercmd.ControllerCom
 	}
 }
 
-// Starts a single gather, main responsibility is loading in the necessary configs.
-func runGatherAndUpload(operator *controller.GatherJob,
-	cfg *controllercmd.ControllerCommandConfig) func(cmd *cobra.Command, _ []string) {
+func runGather(operator *controller.GatherJob, cfg *controllercmd.ControllerCommandConfig) func(cmd *cobra.Command, _ []string) {
 	return func(cmd *cobra.Command, _ []string) {
-		if configArg := cmd.Flags().Lookup("config").Value.String(); len(configArg) == 0 {
-			klog.Exit("error: --config is required")
-		}
-		unstructured, _, _, err := cfg.Config()
-		if err != nil {
-			klog.Exit(err)
-		}
-		cont, err := config.LoadConfig(operator.Controller, unstructured.Object, config.ToDisconnectedController)
-		if err != nil {
-			klog.Exit(err)
-		}
-		operator.Controller = cont
+		clientConfig, protoConfig := createClientConfig(cmd, operator, cfg)
 
-		var clientConfig *rest.Config
-		if kubeConfigPath := cmd.Flags().Lookup("kubeconfig").Value.String(); len(kubeConfigPath) > 0 {
-			kubeConfigBytes, err := os.ReadFile(kubeConfigPath) //nolint: govet
-			if err != nil {
-				klog.Exit(err)
-			}
-			kubeConfig, err := clientcmd.NewClientConfigFromBytes(kubeConfigBytes)
-			if err != nil {
-				klog.Exit(err)
-			}
-			clientConfig, err = kubeConfig.ClientConfig()
-			if err != nil {
-				klog.Exit(err)
-			}
-		} else {
-			clientConfig, err = rest.InClusterConfig()
-			if err != nil {
-				klog.Exit(err)
-			}
-		}
-		protoConfig := rest.CopyConfig(clientConfig)
-		protoConfig.AcceptContentTypes = pbAcceptContentTypes
-		protoConfig.ContentType = pbContentType
-
+		// Before the start of gathering we need to check if featureGates are enabled
 		ctx, cancel := context.WithTimeout(context.Background(), operator.Interval)
+		checkFeatureGates(ctx, clientConfig, operator)
 
-		// before calling the Gather logic, this command must check the featuregate status.
-		configClient, err := configv1client.NewForConfig(clientConfig)
-		if err != nil {
-			klog.Exitf("error: building client: %v", err)
-		}
-		configInformers := configv1informers.NewSharedInformerFactory(configClient, 10*time.Minute)
-
-		desiredVersion := missingVersion
-		if envVersion, exists := os.LookupEnv("RELEASE_VERSION"); exists {
-			desiredVersion = envVersion
-		}
-		// By default, this will exit(0) the process if the featuregates ever change to a different set of values.
-		featureGateAccessor := featuregates.NewFeatureGateAccess(
-			desiredVersion, missingVersion,
-			configInformers.Config().V1().ClusterVersions(), configInformers.Config().V1().FeatureGates(),
-			events.NewLoggingEventRecorder("insights-gather", clock.RealClock{}),
-		)
-		go featureGateAccessor.Run(ctx)
-		go configInformers.Start(ctx.Done())
-
-		select {
-		case <-featureGateAccessor.InitialFeatureGatesObserved():
-			featureGates, _ := featureGateAccessor.CurrentFeatureGates()
-			klog.Infof("FeatureGates initialized: knownFeatureGates=%v", featureGates.KnownFeatures())
-		case <-time.After(1 * time.Minute):
-			klog.Errorf("timed out waiting for FeatureGate detection")
-			klog.Exit(fmt.Errorf("timed out waiting for FeatureGate detection"))
+		// Run gatherer
+		if err := operator.Gather(ctx, clientConfig, protoConfig); err != nil {
+			klog.Error(err)
 		}
 
-		featureGates, err := featureGateAccessor.CurrentFeatureGates()
-		if err != nil {
-			klog.Exit(err)
-		}
-		operator.InsightsConfigAPIEnabled = featureGates.Enabled(features.FeatureGateInsightsConfigAPI)
-		operator.RuntimeExtractorEnabled = featureGates.Enabled(features.FeatureGateInsightsRuntimeExtractor)
+		cancel()
+		os.Exit(0)
+	}
+}
 
-		err = operator.GatherAndUpload(clientConfig, protoConfig)
-		if err != nil {
+// Starts a single gather, main responsibility is loading in the necessary configs.
+func runGatherAndUpload(
+	operator *controller.GatherJob, cfg *controllercmd.ControllerCommandConfig,
+) func(cmd *cobra.Command, _ []string) {
+	return func(cmd *cobra.Command, _ []string) {
+		clientConfig, protoConfig := createClientConfig(cmd, operator, cfg)
+
+		// Before the start of gathering we need to check if featureGates are enabled
+		ctx, cancel := context.WithTimeout(context.Background(), operator.Interval)
+		checkFeatureGates(ctx, clientConfig, operator)
+
+		// Run gatherer
+		if err := operator.GatherAndUpload(clientConfig, protoConfig); err != nil {
 			klog.Exit(err)
 		}
 		cancel()
 		os.Exit(0)
 	}
+}
+
+func checkFeatureGates(ctx context.Context, clientConfig *rest.Config, operator *controller.GatherJob) {
+	configClientSet, err := configv1client.NewForConfig(clientConfig)
+	if err != nil {
+		klog.Exitf("error: building client: %v", err)
+	}
+	configInformers := configv1informers.NewSharedInformerFactory(configClientSet, 10*time.Minute)
+
+	desiredVersion := missingVersion
+	if envVersion, exists := os.LookupEnv("RELEASE_VERSION"); exists {
+		desiredVersion = envVersion
+	}
+
+	// By default, this will exit(0) the process if the featuregates ever change to a different set of values.
+	featureGateAccessor := featuregates.NewFeatureGateAccess(
+		desiredVersion, missingVersion,
+		configInformers.Config().V1().ClusterVersions(), configInformers.Config().V1().FeatureGates(),
+		events.NewLoggingEventRecorder("insights-gather", clock.RealClock{}),
+	)
+	go featureGateAccessor.Run(ctx)
+	go configInformers.Start(ctx.Done())
+
+	select {
+	case <-featureGateAccessor.InitialFeatureGatesObserved():
+		featureGates, _ := featureGateAccessor.CurrentFeatureGates()
+		klog.Infof("FeatureGates initialized: knownFeatureGates=%v", featureGates.KnownFeatures())
+	case <-time.After(1 * time.Minute):
+		klog.Errorf("timed out waiting for FeatureGate detection")
+		klog.Exit(fmt.Errorf("timed out waiting for FeatureGate detection"))
+	}
+
+	featureGates, err := featureGateAccessor.CurrentFeatureGates()
+	if err != nil {
+		klog.Exit(err)
+	}
+
+	operator.InsightsConfigAPIEnabled = featureGates.Enabled(features.FeatureGateInsightsConfigAPI)
+	operator.RuntimeExtractorEnabled = featureGates.Enabled(features.FeatureGateInsightsRuntimeExtractor)
+}
+
+func createClientConfig(
+	cmd *cobra.Command, operator *controller.GatherJob, cfg *controllercmd.ControllerCommandConfig,
+) (clientConfig, protoConfig *rest.Config) {
+	if configArg := cmd.Flags().Lookup("config").Value.String(); len(configArg) == 0 {
+		klog.Exit("error: --config is required")
+	}
+
+	unstructured, _, _, err := cfg.Config()
+	if err != nil {
+		klog.Exit(err)
+	}
+
+	cont, err := config.LoadConfig(operator.Controller, unstructured.Object, config.ToDisconnectedController)
+	if err != nil {
+		klog.Exit(err)
+	}
+	operator.Controller = cont
+
+	var clientCfg *rest.Config
+	if kubeConfigPath := cmd.Flags().Lookup("kubeconfig").Value.String(); len(kubeConfigPath) > 0 {
+		kubeConfigBytes, err := os.ReadFile(kubeConfigPath) //nolint: govet
+		if err != nil {
+			klog.Exit(err)
+		}
+
+		kubeConfig, err := clientcmd.NewClientConfigFromBytes(kubeConfigBytes)
+		if err != nil {
+			klog.Exit(err)
+		}
+
+		clientCfg, err = kubeConfig.ClientConfig()
+		if err != nil {
+			klog.Exit(err)
+		}
+	} else {
+		clientCfg, err = rest.InClusterConfig()
+		if err != nil {
+			klog.Exit(err)
+		}
+	}
+
+	protoCfg := rest.CopyConfig(clientCfg)
+	protoCfg.AcceptContentTypes = pbAcceptContentTypes
+	protoCfg.ContentType = pbContentType
+
+	return clientCfg, protoCfg
 }
