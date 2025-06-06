28d40e74c187b4b126f38440cd0cb525bf944bc5
Ondrej Pokorny <opokorny@redhat.com>
2025-06-06 14:46:50 +0200

    feat: copy proxy envs from IO pod to gathering pods
    This commit adds functionality to copy the cluster-wide
    proxy environment variables from the IO pod to the gathering
    pod, ensuring the proxy is used for uploads from gathering
    pods as well.
    
    Signed-off-by: Ondrej Pokorny <opokorny@redhat.com>

--- pkg/controller/periodic/job_test.go
+++ pkg/controller/periodic/job_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	v1 "k8s.io/api/core/v1"
 	kubefake "k8s.io/client-go/kubernetes/fake"
 )
 
@@ -35,6 +36,62 @@ func TestCreateGathererJob(t *testing.T) {
 			// we mount to volumes
 			assert.Len(t, createdJob.Spec.Template.Spec.Containers[0].VolumeMounts, 2)
 			assert.Equal(t, tt.volumeMountPath, createdJob.Spec.Template.Spec.Containers[0].VolumeMounts[0].MountPath)
+		})
+	}
+}
+
+func TestCreateEnvVar(t *testing.T) {
+	tests := []struct {
+		name        string
+		expectedEnv []v1.EnvVar
+	}{
+		{
+			name: "without-proxy-configuration",
+			expectedEnv: []v1.EnvVar{
+				{
+					Name:  "RELEASE_VERSION",
+					Value: "test-version",
+				},
+				{
+					Name:  "DATAGATHER_NAME",
+					Value: "without-proxy-configuration",
+				},
+			},
+		},
+		{
+			name: "with-proxy-configuration",
+			expectedEnv: []v1.EnvVar{
+				{
+					Name:  "HTTP_PROXY",
+					Value: "http://test-proxy.com",
+				},
+				{
+					Name:  "HTTPS_PROXY",
+					Value: "https://test-proxy.com",
+				},
+				{
+					Name:  "NO_PROXY",
+					Value: "http://test-no-proxy.com",
+				},
+				{
+					Name:  "RELEASE_VERSION",
+					Value: "test-version",
+				},
+				{
+					Name:  "DATAGATHER_NAME",
+					Value: "with-proxy-configuration",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for _, env := range tt.expectedEnv {
+				t.Setenv(env.Name, env.Value)
+			}
+
+			assert.Equal(t, tt.expectedEnv, createEnvVar(tt.name))
 		})
 	}
 }
