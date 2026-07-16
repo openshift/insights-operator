7538d6a893e7fb75bcac8aa3a85262b59e8b9c01
Ondrej Pokorny <opokorny@redhat.com>
2026-07-16 12:00:57 +0200

    feat: add alertmanager configuration gatherer
    Collect anonymized Alertmanager routing
    configuration (receivers, route, inhibit_rules)
    from the alertmanager-main secret in
    openshift-monitoring. Sensitive fields matching
    token, key, secret, password, auth, url, and
    host patterns are anonymized before archiving.
    
    Signed-off-by: Ondrej Pokorny <opokorny@redhat.com>

--- pkg/gatherers/clusterconfig/clusterconfig_gatherer.go
+++ pkg/gatherers/clusterconfig/clusterconfig_gatherer.go
@@ -27,6 +27,7 @@ type gathererFuncPtr = func(*Gatherer, context.Context) ([]record.Record, []erro
 
 var gatheringFunctions = map[string]gathererFuncPtr{
 	"active_alerts":                    (*Gatherer).GatherActiveAlerts,
+	"alertmanager_config":              (*Gatherer).GatherAlertmanagerConfig,
 	"aggregated_monitoring_cr_names":   (*Gatherer).GatherAggregatedMonitoringCRNames,
 	"authentication":                   (*Gatherer).GatherClusterAuthentication,
 	"certificate_signing_requests":     (*Gatherer).GatherCertificateSigningRequests,
